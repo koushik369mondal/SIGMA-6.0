4dbce9177523202b78edead10bcb3b7a9c119495
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-05 10:08:34 +0530

    Complete second practice question(Popcorn Price) by size

--- DELTA/JavaScript/app.js
+++ DELTA/JavaScript/app.js
@@ -60,3 +60,21 @@ if(light == "green"){
     console.log("Go");
 }
 
+//Practice Qs(Popcorn price)
+console.log("Practice Qs(Popcorn price)")
+let size = "M";
+if(size == "XL"){
+    console.log("Price is 250");
+}
+else if(size == "L"){
+    console.log("Price is 200")
+}
+else if(size == "M"){
+    console.log("Price is 100")
+}
+else if(size == "S"){
+    console.log("Price is 50")
+}
+else{
+    console.log("Not Defined")
+}
