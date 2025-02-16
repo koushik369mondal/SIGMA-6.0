f5b5109e1b3a2e0956cb588a915ba34c2f8ff45b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-16 16:08:52 +0530

    query selector

---
+++ DELTA/JavaScript/JS DOM/app.js
@@ -0,0 +1,10 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+// for (let i = 0; i < smallImages.length; i++){
+//     smallImages[i].src = "assets/spiderman_img.png";
+//     console.log(`value of image no. ${i} is changed.`);
+// }
+
+// console.dir(document.querySelector("h1"));
+// console.dir(document.querySelector("#description"));
+// console.dir(document.querySelector(".oldImg"));
+
