d246a7baf0947f1e51bf1223c31ba95a021afa6c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-18 19:19:40 +0530

    More Events

--- DELTA/JavaScript/JS DOM/app.js
+++ DELTA/JavaScript/JS DOM/app.js
@@ -143,23 +143,43 @@
 //     }
 // })
 
-let form = document.querySelector("form");
+// let form = document.querySelector("form");
 
-form.addEventListener("submit", function(event){
-    event.preventDefault();
+// form.addEventListener("submit", function(event){
+//     event.preventDefault();
     // alert("form submitted");
-    console.dir(form);
+    // console.dir(form);
 
-    // let user = document.querySelector("#user");
     // let pass = document.querySelector("#pass");
-    let user = this.elements[0];
-    let pass = this.elements[1];
+    // let user = this.elements[0];
+    // let pass = this.elements[1];
 
-    console.log(user.value);
-    console.log(pass.value);
-    console.log(`Hi ${user.value} your password set to ${pass.value}.`);
+    // console.log(user.value);
+    // console.log(pass.value);
+    // console.log(`Hi ${user.value} your password set to ${pass.value}.`);
 
     // console.dir(inp);
     // console.log(inp.value);
-})
+// });
 
+// let user = document.querySelector("#user");
+
+// user.addEventListener("change", function(){
+//     console.log("change event");
+//     console.log("final value", this.value);
+// });
+
+// user.addEventListener("input", function(){
+//     console.log("input event");
+//     console.log("final value", this.value);
+// });
+
+let inp = document.querySelector("input");
+
+let p = document.querySelector("p");
+
+inp.addEventListener("input", function(){
+    // h1.textContent = this.value;
+    console.log(inp.value);
+    p.innerText = inp.value;
+});
