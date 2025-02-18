7ab68344d7c9b4176725f4656914f6c71d51a1c1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-18 10:04:56 +0530

    onclick on html

--- DELTA/JavaScript/JS DOM/Assignment/app.js
+++ DELTA/JavaScript/JS DOM/Assignment/app.js
@@ -1,4 +1,30 @@
 
+// Practice Qs 
+let p1 = document.createElement('p');
+p1.textContent = "Hello I'm red!";
+// p.style.color = "red";
+p1.classList.add('red');
+document.querySelector("body").append(p1);
+
+let h3 = document.createElement('h3');
+h3.textContent = "I'm blue h3";
+document.querySelector("body").append(h3);
+h3.classList.add('blue');
+
+let div = document.createElement('div');
+let h1 = document.createElement('h1');
+let p2 = document.createElement('p');
+// div.style.border = "2px";
+h1.textContent = "I' in a div";
+p2.textContent = "ME TOO!";
+// document.querySelector("div").append(h1);
+// document.querySelector("div").append(p2);
+div.append(h1);
+div.append(p2);
+div.classList.add('box');
+document.querySelector("body").append(div);
+
+
 // Qs 1
 let input = document.createElement('input');
 let button = document.createElement('button');
