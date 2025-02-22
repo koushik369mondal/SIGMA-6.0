543d1da064bf0782bd69c776535e97c74fb4d49d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-22 10:43:29 +0530

    gameFlash turn into white and userFlash turn into green background color

--- DELTA/JavaScript/SimonSayGame/app.js
+++ DELTA/JavaScript/SimonSayGame/app.js
@@ -1,12 +1,57 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["yellow", "red", "purple", "green"];
+
 let started = false;
 let level = 0;
+
+let h2 = document.querySelector("h2");
 
 document.addEventListener("keypress", function(){
     if(started == false){
         console.log("game is started");
         started = true;
+
+        levelUp();
     }
 });
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    }, 250);
+}
+
+function userFlash(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+    }, 250);
+}
+
+function levelUp(){
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    let randIdx = Math.floor(Math.random() * 3);
+    let randomColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randomColor}`);
+
+    console.log(randIdx);
+    console.log(randomColor);
+    console.log(randBtn);
+    gameFlash(randBtn);
+}
+
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click", btnPress);
+}
