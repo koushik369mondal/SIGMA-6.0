632ae53569bfb3069490e897d3911a410ecd2b26
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-22 19:40:34 +0530

    Callback Hell

--- DELTA/JavaScript/practice/app.js
+++ DELTA/JavaScript/practice/app.js
@@ -993,3 +993,39 @@ function three(){
     console.log(ans);
 }
 console.log(three());
+
+// JS is Single Threaded
+// it is asynchronous because it follow step by step
+
+h1 = document.querySelector("h1");
+
+function changeColor(color, delay, nextColorChange){
+    setTimeout(() =>{
+        h1.style.color = color;
+        if(nextColorChange) nextColorChange();
+    }, delay);
+}
+
+changeColor("red", 1000, () =>{
+    changeColor("orange", 1000, () =>{
+        changeColor("green", 1000, () =>{
+            changeColor("blue", 1000);
+        });
+    });
+});
+
+
+// changeColor("orange", 2000);
+// changeColor("green", 3000);
+
+// setTimeout(() =>{
+//     h1.style.color = "red";
+// }, 1000);
+
+// setTimeout(() =>{
+//     h1.style.color = "orange";
+// }, 2000);
+
+// setTimeout(() =>{
+//     h1.style.color = "green";
+// }, 3000);
