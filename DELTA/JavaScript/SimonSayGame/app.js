a613978305a0141786f63604505dcdcb371971da
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-22 16:35:59 +0530

    game complete

--- DELTA/JavaScript/SimonSayGame/app.js
+++ DELTA/JavaScript/SimonSayGame/app.js
@@ -32,26 +32,55 @@ function userFlash(btn){
 }
 
 function levelUp(){
+    userSeq = [];
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 3);
+    let randIdx = Math.floor(Math.random() * 4);
     let randomColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randomColor}`);
-
-    console.log(randIdx);
-    console.log(randomColor);
-    console.log(randBtn);
+    // console.log(randIdx);
+    // console.log(randomColor);
+    // console.log(randBtn);
+    gameSeq.push(randomColor);
+    console.log(gameSeq);
     gameFlash(randBtn);
 }
 
+function checkAns(idx){
+    // console.log("curr level: ", level);
+    // let idx = level -1;
+
+    if(userSeq[idx] === gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+    }
+    else{
+        h2.innerText = `Game Over! press any key to start`;
+        reset();
+    }
+}
+
 function btnPress() {
-    console.log(this);
+    // console.log(this);
     let btn = this;
     userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length -1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click", btnPress);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
 }
