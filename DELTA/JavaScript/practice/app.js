82b008049a82bf3dc89b602a548c03ddbb4ae78f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-24 18:07:44 +0530

    async Functions

--- DELTA/JavaScript/practice/app.js
+++ DELTA/JavaScript/practice/app.js
@@ -995,33 +995,33 @@
 // JS is Single Threaded
 // it is asynchronous because it follow step by step
 
-h1 = document.querySelector("h1");
+// h1 = document.querySelector("h1");
 
-function changeColor(color, delay){
-    return new Promise((resolve, reject) => {
-        setTimeout(() =>{
-            h1.style.color = color;
-            resolve("color changed");
-        }, delay);
-    })
-}
+// function changeColor(color, delay){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() =>{
+//             h1.style.color = color;
+//             resolve("color changed");
+//         }, delay);
+//     })
+// }
 
-changeColor("red", 1000)
-.then(() => {
-    console.log("red color was completed");
-    return changeColor("orange", 1000);
-})
-.then(() => {
-    console.log("orange color was completed");
-    return changeColor("green", 1000);
-})
-.then(() => {
-    console.log("green color was completed");
-    return changeColor("blue", 1000);
-})
-.then(() => {
-    console.log("blue color was completed");
-})
+// changeColor("red", 1000)
+// .then(() => {
+//     console.log("red color was completed");
+//     return changeColor("orange", 1000);
+// })
+// .then(() => {
+//     console.log("orange color was completed");
+//     return changeColor("green", 1000);
+// })
+// .then(() => {
+//     console.log("green color was completed");
+//     return changeColor("blue", 1000);
+// })
+// .then(() => {
+//     console.log("blue color was completed");
+// })
 
 // changeColor("red", 1000, () =>{
 //     changeColor("orange", 1000, () =>{
@@ -1127,3 +1127,22 @@ changeColor("red", 1000)
 //         console.log("promise was rejected");
 //         console.log("result of promise: ",error);
 //     });
+
+async function greet() {
+    // abc .abc();
+    // throw "some random error";
+    return "hello";
+}
+
+greet()
+.then((result) =>{
+    console.log("promise was resolved");
+    console.log("result was: ", result);
+})
+.catch((err) =>{
+    console.log("promise was rejected with err: ", err);
+});
+
+let demo = async () => {
+    return 5;
+};
