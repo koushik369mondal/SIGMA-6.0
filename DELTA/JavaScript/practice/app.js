5ddaf840927a4583b64a924dc0666b3fbbaacf28
Kaushik Mandal <koushik369mondal@gmail.com>
2025-02-25 20:29:23 +0530

    Using Fetch with async-await

--- DELTA/JavaScript/practice/app.js
+++ DELTA/JavaScript/practice/app.js
@@ -1204,22 +1204,39 @@
 // https://hoppscotch.io/
 
 let url = "https://catfact.ninja/fact";
-fetch(url)
-.then((res) => {
-    return res.json();
-})
-.then((data) => {
-    console.log("data1 = ",data.fact);
-    return fetch(url);
-})
-.then((res) => {
-    return res.json();
-})
-.then((data2) => {
-    console.log("data2 = ",data2.fact);
-})
-.catch((err) => {
-    console.log("ERROR", err);
-});
+// fetch(url)
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data) => {
+//     console.log("data1 = ",data.fact);
+//     return fetch(url);
+// })
+// .then((res) => {
+//     return res.json();
+// })
+// .then((data2) => {
+//     console.log("data2 = ",data2.fact);
+// })
+// .catch((err) => {
+//     console.log("ERROR", err);
+// });
 
-console.log("I am happy");
+// console.log("I am happy");
+
+async function getfacts() {
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    }
+    catch(e){
+        console.log("Error", e);
+    }
+
+    console.log("bye")
+}
