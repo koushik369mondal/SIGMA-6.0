236c727897e49e055e36a347a145e4386f5aa33b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-04 23:43:33 +0530

    Installing Packages

---
+++ DELTA/Backend/FigletDir/index.js
@@ -0,0 +1,10 @@
+const figlet = require('figlet');
+
+figlet("Kaushik Mandal", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
