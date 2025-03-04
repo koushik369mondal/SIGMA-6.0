029c369fc963b329df9cd3392b01b06c5247f57b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-04 20:16:43 +0530

    Export in Files

---
+++ DELTA/Backend/math.js
@@ -0,0 +1,33 @@
+
+// const sum = (a, b) => a+b;
+// const mul = (a, b) => a*b;
+// const g = 9.8;
+// const Pi = 3.14;
+
+// module.exports.sum = (a, b) => a+b;
+// module.exports.mul = (a, b) => a*b;
+// module.exports.g = 9.8;
+// module.exports.Pi = 3.14;
+
+// exports.sum = (a, b) => a+b;
+// exports.mul = (a, b) => a*b;
+// exports.g = 9.8;
+// exports.Pi = 3.14;
+
+// module.exports = 123;
+
+// let obj = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     Pi: Pi
+// }
+
+// module.exports = obj;
+
+// module.exports = {
+//     sum: sum,
+//     mul: mul,
+//     g: g,
+//     Pi: Pi
+// }
--- DELTA/Backend/script.js
+++ DELTA/Backend/script.js
@@ -5,8 +5,15 @@
 
 // console.log(process.argv);
 
-let args = process.argv;
+// let args = process.argv;
 
-for(let i=2; i<args.length; i++){
-    console.log("Hello and welcome to ",args[i]);
-}
+// for(let i=2; i<args.length; i++){
+//     console.log("Hello and welcome to ",args[i]);
+// }
+
+const someValue = require("./math");
+
+console.log(someValue);
+
+console.log(someValue.sum(2, 3));
+console.log(someValue.Pi);
