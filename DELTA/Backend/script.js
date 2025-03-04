b24cf6e7886a759b9809afff35cef62dc05f141c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-04 19:35:45 +0530

    Process in Node

--- DELTA/Backend/script.js
+++ DELTA/Backend/script.js
@@ -1,4 +1,12 @@
-let n = 5;
-for(let i=0; i<n; i++){
-    console.log("Hello", i);
+// let n = 5;
+// for(let i=0; i<n; i++){
+//     console.log("Hello", i);
+// }
+
+// console.log(process.argv);
+
+let args = process.argv;
+
+for(let i=2; i<args.length; i++){
+    console.log("Hello and welcome to ",args[i]);
 }
