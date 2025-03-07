3fcb9a39e3a8df2fb313f2d089e5b865d65b65f8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 02:03:11 +0530

    Routing

--- DELTA/Backend/ExpressDir/index.js
+++ DELTA/Backend/ExpressDir/index.js
@@ -6,16 +6,36 @@ const app = express();
 let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
-})
+});
 
-app.use((req, res) => {
+app.get('/', (req, res) => {
+    res.send("you contacted to root path");
+});
+
+app.get('/apple', (req, res) => {
+    res.send("you contacted to apple path");
+});
+
+app.get('/orange', (req, res) => {
+    res.send("you contacted to orange path");
+});
+
+app.get("*", (req, res) => {
+    res.send("This path does not exist");
+});
+
+app.post("/", (req, res) => {
+    res.send("you sent a POST request to root path");
+});
+
+// app.use((req, res) => {
     // console.log(req);
-    console.log("request received");
+    // console.log("request received");
     // res.send({
     //     name: "Apple",
     //     color: "Red",
     // });
     
-    let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Banana</li><li>Orange</li></ul>";
-    res.send(code);
-});
+//     let code = "<h1>Fruits</h1> <ul><li>Apple</li><li>Banana</li><li>Orange</li></ul>";
+//     res.send(code);
+// });
