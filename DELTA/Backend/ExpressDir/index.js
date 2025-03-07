006dc17af23066d8cacfeb47bd25e874a66e0736
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-07 10:36:37 +0530

    Handling requests

--- DELTA/Backend/ExpressDir/index.js
+++ DELTA/Backend/ExpressDir/index.js
@@ -3,7 +3,11 @@ const express = require('express');
 const app = express();
 // console.log(app);
 
-let port = 8080;
+let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
+})
+
+app.use((req, res) => {
+    console.log("request received");
 })
