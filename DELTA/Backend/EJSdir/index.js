3a6c6dba57bb7897590f0b2195eec180c48fc67b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 11:02:25 +0530

    using EJS

--- DELTA/Backend/EJSdir/index.js
+++ DELTA/Backend/EJSdir/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 let port = 8081;
 
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    // res.send("This is home page");
+    res.render("home.ejs");
+});
+
+app.get("/hello", (req, res) => {
+    res.send("Hello");
+});
 app.listen(port, () => {
     console.log(`Listning on port ${port}...`);
 });
