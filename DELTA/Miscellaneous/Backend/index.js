ebcab3a30c6618cc8f19e60e613ab098eb3db3a5
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 23:25:07 +0530

    GET & POST Request

---
+++ DELTA/Miscellaneous/Backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get("/register", (req, res) => {
+    let {user, password } = req.query;
+    res.send(`standard GET response. Welcome ${user}!`);
+});
+
+app.post("/register", (req, res) => {
+    res.send("standard POST response.")
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
