b2c6aaa884e4a2cb674d1a6c24b03dc83a276ff5
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 11:05:18 +0530

    Views Directory

--- DELTA/Backend/EJSdir/index.js
+++ DELTA/Backend/EJSdir/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require('path'); //set path to run from backend
 
 let port = 8081;
 
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname, "views"));
+app.set("views",path.join(__dirname, "views")); //set this to access from backend
 
 app.get("/", (req, res) => {
     // res.send("This is home page");
