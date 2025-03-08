5c6c714cd5dbb3edc894fc67035e60d7c8c137fc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 17:48:26 +0530

    Instagram EJS

--- DELTA/Backend/EJSdir/index.js
+++ DELTA/Backend/EJSdir/index.js
@@ -12,6 +12,12 @@ app.get("/", (req, res) => {
     res.render("home.ejs");
 });
 
+app.get("/ig/:username", (req, res) => {
+    let {username } = req.params;
+    // console.log(`Hello, ${username}!`);
+    res.render("instagram.ejs", {username});
+})
+
 app.get("/hello", (req, res) => {
     res.send("Hello");
 });
