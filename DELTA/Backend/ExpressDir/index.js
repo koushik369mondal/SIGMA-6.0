6d57eb096bae7785ffea341bdf06ca9a977cd0ab
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-08 09:34:34 +0530

    Path Parameters

--- DELTA/Backend/ExpressDir/index.js
+++ DELTA/Backend/ExpressDir/index.js
@@ -1,9 +1,8 @@
-
 const express = require('express');
 const app = express();
 // console.log(app);
 
-let port = 3000;
+let port = 8080;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
@@ -12,21 +11,30 @@ app.get('/', (req, res) => {
     res.send("Hello I am root");
 });
 
-app.get('/apple', (req, res) => {
-    res.send("you contacted to apple path");
+app.get('/:username/:id', (req, res) => {
+    // console.log(req.params);
+    let {username, id } = req.params;
+    let htmlString = `<h1>Welcome ${username} with id ${id} to the root path!</h1>`;
+    res.send(htmlString);
+    // res.send(`Welcome ${username} with id ${id} to the root path!`);
 });
 
-app.get('/orange', (req, res) => {
-    res.send("you contacted to orange path");
-});
 
-app.get("*", (req, res) => {
-    res.send("This path does not exist");
-});
+// app.get('/apple', (req, res) => {
+//     res.send("you contacted to apple path");
+// });
 
-app.post("/", (req, res) => {
-    res.send("you sent a POST request to root path");
-});
+// app.get('/orange', (req, res) => {
+//     res.send("you contacted to orange path");
+// });
+
+// app.get("*", (req, res) => {
+//     res.send("This path does not exist");
+// });
+
+// app.post("/", (req, res) => {
+//     res.send("you sent a POST request to root path");
+// });
 
 // app.use((req, res) => {
     // console.log(req);
