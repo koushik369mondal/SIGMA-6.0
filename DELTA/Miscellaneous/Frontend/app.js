0ff57076f1edccb8270e04b94bef4e40e6a24239
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-09 10:43:40 +0530

    Factory Functions

--- DELTA/Miscellaneous/Frontend/app.js
+++ DELTA/Miscellaneous/Frontend/app.js
@@ -1,16 +1,30 @@
-let arr1 = [1,2,3,4,5,6,7,8,9,10];
-let arr2 = [1,2,3,4,5,6,7,8,9,10];
+// let arr1 = [1,2,3,4,5,6,7,8,9,10];
+// let arr2 = [1,2,3,4,5,6,7,8,9,10];
 
-arr1.sayHello = () => {
-    console.log("Hello, I am array");
-};
+// arr1.sayHello = () => {
+//     console.log("Hello, I am array");
+// };
 
-arr2.sayHello = () => {
-    console.log("Hello, I am array");
-};
+// arr2.sayHello = () => {
+//     console.log("Hello, I am array");
+// };
 
 // arr1.sayHello == arr2.sayHello
 // false
 
 // "abc".toLocaleUpperCase == "xyz".toLocaleUpperCase
 // true
+
+function personMaker(name, age){
+    const person = {
+        name : name,
+        age : age,
+        talk() {
+            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+        },
+    };
+    return person;
+}
+
+let p1 = personMaker("kaushik", 20);
+let p2 = personMaker("Gaurav", 39);
