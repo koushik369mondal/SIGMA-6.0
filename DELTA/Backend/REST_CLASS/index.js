2649915d0b5a2ae2fa0f0618ddd9da67164dec4a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-11 23:34:23 +0530

    Index Route

--- DELTA/Backend/REST_CLASS/index.js
+++ DELTA/Backend/REST_CLASS/index.js
@@ -8,11 +8,26 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'));
 
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-    res.send("Server working well!");
-})
+let posts = [
+    {
+        username: "apnacollage",
+        content: "I love coding"
+    },
+    {
+        username: "kaushikmandal",
+        content: "Hard work is important"
+    },
+    {
+        username: "random",
+        content: "I got seected with"
+    },
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs",{posts});
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
