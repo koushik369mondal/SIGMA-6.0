4fa3caeda4dffd169842dd85706d9f522d5c1102
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-12 01:25:07 +0530

    Create & New Route

--- DELTA/Backend/REST_CLASS/index.js
+++ DELTA/Backend/REST_CLASS/index.js
@@ -4,6 +4,7 @@ const port = 8080;
 const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, 'views'));
@@ -27,6 +28,16 @@ let posts = [
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs",{posts});
+});
+
+app.get("/posts/new", (req, res) =>{
+    res.render("new.ejs");
+});
+
+app.post('/posts', (req, res) => {
+    let {username, content} = req.body;
+    posts.push({username, content});
+    res.send("Post request working");
 });
 
 app.listen(port, () => {
