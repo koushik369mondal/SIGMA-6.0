74670f61f82144e35effb05a2ed47e88ed45a15c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-16 23:56:17 +0530

    Update Route

--- DELTA/Backend/SQLClass/index.js
+++ DELTA/Backend/SQLClass/index.js
@@ -76,7 +76,28 @@ app.get("/user/:id/edit", (req, res) => {
 
 //Update (DB) Route
 app.patch("/user/:id", (req, res) => {
-    res.send("updated");
+    let {id} = req.params;
+    let {password: formPass, username: newUsername} = req.body;
+    let q = `SELECT * FROM user WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if(formPass != user.password){
+                res.send("WRONG Password");
+            } else {
+                let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+                connection.query(q2, (err, result) => {
+                    if(err) throw err;
+                    res.redirect("/user");
+                });
+            }
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+    }
 });
 
 app.listen("8080", () => {
