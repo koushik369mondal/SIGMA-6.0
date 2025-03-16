2efed01c8d7d099113451097994a3ba625a5ff5b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-16 22:35:03 +0530

    Edit Route

--- DELTA/Backend/SQLClass/index.js
+++ DELTA/Backend/SQLClass/index.js
@@ -3,7 +3,10 @@ const mysql = require('mysql2');
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -52,6 +55,28 @@ app.get("/user", (req, res) => {
         console.log(err);
         res.send("some error in DB");
     }
+});
+
+//Edit Route
+app.get("/user/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let q = `SELECT * FROM user WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            res.render("edit.ejs", {user});
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.send("some error in DB");
+    }
+});
+
+//Update (DB) Route
+app.patch("/user/:id", (req, res) => {
+    res.send("updated");
 });
 
 app.listen("8080", () => {
