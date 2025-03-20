e855e6c2e2dc09a514f06cb8580664b54c46b54d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-20 23:49:39 +0530

    add mongoose path

---
+++ DELTA/Backend/MONGO/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+main()
+    .then((res) => {
+        console.log('Connection Successful');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+}
+
