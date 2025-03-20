fc37ee1fc7802f8d1faffa900601c7e561f8d294
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-21 00:00:19 +0530

    create schema

--- DELTA/Backend/MONGO/index.js
+++ DELTA/Backend/MONGO/index.js
@@ -13,3 +13,8 @@ async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
 
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+})
