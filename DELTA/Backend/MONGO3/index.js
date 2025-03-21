971b59a6f7aebe3fbfae210af434889ff3906853
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-21 23:16:09 +0530

    connect to MongoDB and update root route handler

--- DELTA/Backend/MONGO3/index.js
+++ DELTA/Backend/MONGO3/index.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-    res.send('root is working');
-})
+main()
+    .then(() => {
+        console.log("Connected Successfully");
+    })
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+app.get("/", (req, res) => {
+    res.send("root is working");
+});
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
-})
+    console.log("Server is running on port 8080");
+});
