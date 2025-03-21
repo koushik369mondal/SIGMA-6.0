dc702c51bbb736b1f240f3857667144645393d6c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-21 14:03:52 +0530

    add mongoose connection and book schema definition

---
+++ DELTA/Backend/MONGO/books.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+main()
+    .then((res) => {
+        console.log('Connection Successful');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    price: Number,
+});
