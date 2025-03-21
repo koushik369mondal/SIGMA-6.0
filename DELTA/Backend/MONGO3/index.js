d52d806fa631f4f980aa9ff66521bcd76399d48f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-21 23:08:28 +0530

    add root route handler to Express server

--- DELTA/Backend/MONGO3/index.js
+++ DELTA/Backend/MONGO3/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('root is working');
+})
+
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 })
