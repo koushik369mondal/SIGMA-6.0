749b476ef48db5b8169e5f587ee7304d10040209
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-24 01:57:04 +0530

    add root route to app.js with a greeting message

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+})
+
 app.listen(8080, () => {
     console.log('Server is running on port 8080');
 })
