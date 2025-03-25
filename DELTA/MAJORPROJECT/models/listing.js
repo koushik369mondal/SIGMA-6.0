4c11a26cb207dafd82e5595b7c08cb3bd828b9ab
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-25 19:37:01 +0530

    enhance listing model schema with required fields and default image

--- DELTA/MAJORPROJECT/models/listing.js
+++ DELTA/MAJORPROJECT/models/listing.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: true,
+    },
     description: String,
-    image: String,
+    image: {
+        type: String,
+        set: (v) =>
+            v === ""
+                ? "https://unsplash.com/photos/a-sleek-modern-desk-setup-with-multiple-screens-IO3_vOUwjVs"
+                : v,
+    },
     price: Number,
     location: String,
     country: String,
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
+modules.export = Listing;
