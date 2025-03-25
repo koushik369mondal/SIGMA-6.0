d9212a9578ad4dc6332586877d1f32ab38bc3e9d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-25 20:19:57 +0530

    fix: update default image URL in listing schema and correct module export syntax

--- DELTA/MAJORPROJECT/models/listing.js
+++ DELTA/MAJORPROJECT/models/listing.js
@@ -10,9 +10,9 @@ const listingSchema = new Schema({
     image: {
         type: String,
         set: (v) =>
-            v === ""
-                ? "https://unsplash.com/photos/a-sleek-modern-desk-setup-with-multiple-screens-IO3_vOUwjVs"
-                : v,
+            v === ""  
+                ? "https://images.unsplash.com/photo-1742199009963-c028d0c5a603?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                : v,  
     },
     price: Number,
     location: String,
@@ -20,4 +20,4 @@ const listingSchema = new Schema({
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
-module.export = Listing;
+module.exports = Listing;
