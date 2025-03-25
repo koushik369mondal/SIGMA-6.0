c7dd18efea71d1a7e30eb0e4232b852533839744
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-25 19:26:01 +0530

    add listing model schema for property listings

---
+++ DELTA/MAJORPROJECT/models/listing.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: String,
+    description: String,
+    image: String,
+    price: Number,
+    location: String,
+    country: String,
+});
