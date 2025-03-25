384838ab5784fd9331adbf36335a415543e88253
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-26 02:05:04 +0530

    fix: correct import path for listing model and restructure image schema

--- DELTA/MAJORPROJECT/models/listing.js
+++ DELTA/MAJORPROJECT/models/listing.js
@@ -8,12 +8,11 @@ const listingSchema = new Schema({
     },
     description: String,
     image: {
-        type: String,
-        default: "https://images.unsplash.com/photo-1742199009963-c028d0c5a603?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        set: (v) =>
-            v === ""  
-                ? "https://images.unsplash.com/photo-1742199009963-c028d0c5a603?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                : v,  
+        filename: { type: String, default: "listingimage" },
+        url: { 
+            type: String, 
+            default: "https://images.unsplash.com/photo-1742199009963-c028d0c5a603?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        }
     },
     price: Number,
     location: String,
