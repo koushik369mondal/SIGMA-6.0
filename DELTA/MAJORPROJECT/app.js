cbf7402efce6b51879683242616443c7d4f690e9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-28 01:20:32 +0530

    update listing redirect after successful edit to show updated listing

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -63,7 +63,7 @@ app.get("/listings/:id/edit", async(req, res) => {
 app.put("/listings/:id", async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect("/listings");
+    res.redirect(`/listings/${id}`);
 });
 
 // app.get("/testListing", async (req, res) => {
