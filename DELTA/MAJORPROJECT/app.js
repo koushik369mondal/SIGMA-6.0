04def707a33e57af6f3a11e64740d9bf33105d7b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-28 01:11:47 +0530

    add method-override middleware and implement edit listing functionality

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require('path');
+const methodOverride = require('method-override');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
@@ -20,6 +21,7 @@ async function main() {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
@@ -48,7 +50,14 @@ app.post("/listings", async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect(`/listings`);
-})
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async(req, res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", {listing});
+});
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
