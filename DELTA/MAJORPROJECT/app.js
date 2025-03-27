160793862915119b80183b7ab5058e89b357afff
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-28 01:19:26 +0530

    add update route for listings and fix textarea value binding in edit form

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -59,6 +59,13 @@ app.get("/listings/:id/edit", async(req, res) => {
     res.render("listings/edit.ejs", {listing});
 });
 
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect("/listings");
+});
+
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title: "My New Villa",
