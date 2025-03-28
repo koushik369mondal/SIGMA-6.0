5fb509da920f0a72a902cd217f1424b4d836005e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-28 09:47:33 +0530

    add ejs-mate as a dependency and integrate it into the application

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
