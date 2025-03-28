b0f87c93fa9f4e74ac967c632a1134370e42671d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-28 10:22:30 +0530

    add boilerplate layout for EJS views and integrate ejs-mate engine

--- DELTA/MAJORPROJECT/app.js
+++ DELTA/MAJORPROJECT/app.js
@@ -23,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
