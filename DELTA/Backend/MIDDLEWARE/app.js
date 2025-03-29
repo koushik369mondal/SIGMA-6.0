dad6e1702469712a6b1e9a9c54cccd36f15ab159
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-30 00:43:39 +0530

    500 inter server error

--- DELTA/Backend/MIDDLEWARE/app.js
+++ DELTA/Backend/MIDDLEWARE/app.js
@@ -44,7 +44,7 @@ app.get("/err", (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-    let {status, message} = err;
+    let {status = 500, message} = err;
     res.status(status).send(message);
 });
 
