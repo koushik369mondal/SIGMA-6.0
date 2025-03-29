2dbd0d4140655517ade260ebead28ac918da8b3c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-29 12:51:01 +0530

    implement access control for /api route and deny access without token

--- DELTA/Backend/MIDDLEWARE/app.js
+++ DELTA/Backend/MIDDLEWARE/app.js
@@ -11,9 +11,16 @@ const app = express();
 //     next();
 // });
 
-app.use("/random", (req, res, next) => {
-    console.log("I am only for random");
-    next();
+app.use("/api",(req, res, next) => {
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next();
+    }
+    res.send("ACCESS DENIED!");
+});
+
+app.get("/api", (req, res) => {
+    res.send("data");
 })
 
 app.get('/', (req, res) => {
@@ -25,11 +32,11 @@ app.get("/random", (req, res) => {
 });
 
 // logger -morgan
-app.use((req, res, next) => {
-    req.time = Date.now();
-    console.log(req.method, req.hostname, req.path, req.time);
-    next();
-});
+// app.use((req, res, next) => {
+//     req.time = Date.now();
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// });
 
 //404
 app.use((req, res) => {
