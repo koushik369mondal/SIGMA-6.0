5e6f7b00e07acc9e9423645b9c5e382785553de7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-29 09:05:35 +0530

    http://localhost:8080/random?query=abcd

--- DELTA/Backend/MIDDLEWARE/app.js
+++ DELTA/Backend/MIDDLEWARE/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res) => {
+    let {query} = req.query;
+    console.log('Query:', query);
+    console.log('Hi I am middleware');
+    res.send("Middleware finished");
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+    res.send('Hi I am root');
+});
+
+app.get("/random", (req, res) => {
+    res.send('Random Number:'+ Math.random());
+});
 
 app.listen(8080, () => {
     console.log('Server is running on PORT 8080');
-})
+});
