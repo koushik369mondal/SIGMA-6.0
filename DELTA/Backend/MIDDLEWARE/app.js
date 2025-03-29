e4f95c95288e9b106b8ccfe6eabafed88923073f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-29 10:30:40 +0530

    refactor middleware: comment out unused middleware and log request details

--- DELTA/Backend/MIDDLEWARE/app.js
+++ DELTA/Backend/MIDDLEWARE/app.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Hi I am 1st middleware');
-    return next();
-    console.log("This is after next");
-});
+// app.use((req, res, next) => {
+//     console.log('Hi I am 1st middleware');
+//     next();
+// });
+
+// app.use((req, res, next) => {
+//     console.log('Hi I am 2nd middleware');
+//     next();
+// });
 
 app.use((req, res, next) => {
-    console.log('Hi I am 2nd middleware');
+    req.time = newDate(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, req.time);
     next();
-});
+})
 
 app.get('/', (req, res) => {
     res.send('Hi I am root');
