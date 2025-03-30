e2a4cbc71069dad532a33027af723764db959caa
Kaushik Mandal <koushik369mondal@gmail.com>
2025-03-30 10:13:09 +0530

    add error handling for chat routes to improve stability and user experience

--- DELTA/Backend/MONGO3/index.js
+++ DELTA/Backend/MONGO3/index.js
@@ -24,9 +24,14 @@ async function main() {
 
 //Index Route
 app.get("/chats", async (req, res) => {
-    let chats = await Chat.find();
-    // console.log(chats);
-    res.render("index.ejs", { chats });
+    try{
+        let chats = await Chat.find();
+        res.render("index.ejs", { chats });
+    }
+    catch (err) {
+        next(err);
+    }
+
 });
 
 //New Route
@@ -56,42 +61,63 @@ app.post("/chats", async (req, res, next) => {
 
 //NEW- Show Route
 app.get("/chats/:id", async (req, res, next) => {
-    let { id } = req.params;
-    let chat = await Chat.findById(id);
-    if (!chat) {
-        next(new ExpressError(500, "Chat not found"));
+    try{
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        if (!chat) {
+            next(new ExpressError(500, "Chat not found"));
+        }
+        res.render("edit.ejs", { chat });
     }
-    res.render("edit.ejs", { chat });
+    catch (err) {
+        next(err);
+    }
 });
 
 // Edit Route
 app.get("/chats/:id/edit", async (req, res) => {
-    let { id } = req.params;
-    let chat = await Chat.findById(id);
-    res.render("edit.ejs", { chat });
+    try{
+        let { id } = req.params;
+        let chat = await Chat.findById(id);
+        console.log(chat);
+        res.render("edit.ejs", { chat });
+    }
+    catch (err) {
+        next(err);
+    }
 });
 
 // Update Route
 app.put("/chats/:id", async (req, res) => {
-    let { id } = req.params;
-    let {msg : newMsg } = req.body;
-    console.log(newMsg);
-    let updatedChat = await Chat.findByIdAndUpdate(
-        id,
-        { msg: newMsg },
-        { runValidators: true, new: true }
-    );
-    console.log(updatedChat);
-    res.redirect("/chats");
+    try{
+        let { id } = req.params;
+        let {msg : newMsg } = req.body;
+        console.log(newMsg);
+        let updatedChat = await Chat.findByIdAndUpdate(
+            id,
+            { msg: newMsg },
+            { runValidators: true, new: true }
+        );
+        console.log(updatedChat);
+        res.redirect("/chats");
+    }
+    catch (err) {
+        next(err);
+    }
 });
 
 // Delete Route
 app.delete("/chats/:id", async (req, res) => {
-    let { id } = req.params;
-    let deletedChat = await Chat.findByIdAndDelete(id);
-    console.log(deletedChat);
-    res.redirect("/chats");
-})
+    try{
+        let { id } = req.params;
+        let deletedChat = await Chat.findByIdAndDelete(id);
+        console.log(`Deleted: ${deletedChat}`);
+        res.redirect("/chats");
+    }
+    catch (err) {
+        next(err);
+    }
+});
 
 app.get("/", (req, res) => {
     res.send("root is working");
