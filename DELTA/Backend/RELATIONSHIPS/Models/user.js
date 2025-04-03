f1768e36f003caa3dc737fc4717129f4b9b19a52
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-03 19:06:45 +0530

    npm init -y, npm i express, npm i mongoose

---
+++ DELTA/Backend/RELATIONSHIPS/Models/user.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+main()
+    .then(() => console.log("Connection successful"))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
