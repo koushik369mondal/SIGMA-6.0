b201b446e0003000c2ea940d901ca77013cc5ec6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-04 01:43:50 +0530

    refactor: rename addData function to getData and update logic to retrieve and log a post with populated user

--- DELTA/Backend/RELATIONSHIPS/Models/posts.js
+++ DELTA/Backend/RELATIONSHIPS/Models/posts.js
@@ -26,18 +26,10 @@ const postSchema = new Schema({
 const User = mongoose.model("User", userSchema);
 const Post = mongoose.model("Post", postSchema);
 
-const addData = async () => {
-    let user =await User.findOne({username: "rahulkumar"});
-
-    let post2 = new Post({
-        content: "Bye Bye :)",
-        likes: 23,
-    });
-
-    post2.user = user;
-
-    await post2.save();
+const getData = async () => {
+    let result = await Post.findOne({}).populate("user");
+    console.log(result);
 };
 
-addData();
+getData();
 
