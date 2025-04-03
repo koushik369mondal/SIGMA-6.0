2dcedfd63bb167001f38b84f9268ec68ec6a98d8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-03 19:55:26 +0530

    feat: add user model with schema and example user creation

--- DELTA/Backend/RELATIONSHIPS/Models/user.js
+++ DELTA/Backend/RELATIONSHIPS/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 main()
     .then(() => console.log("Connection successful"))
@@ -7,3 +8,32 @@ main()
 async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
 }
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {
+            location: String,
+            city: String,
+        },
+    ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUser = async () => {
+    let user1 = new User({
+        username: "John Doe",
+        addresses: [
+            {
+                location: "Baker Street",
+                city: "London",
+            },
+        ],
+    });
+    user1.addresses.push({ location: "WallStreet", city: "London" });
+    let result = await user1.save();
+    console.log(result);
+};
+
+addUser();
