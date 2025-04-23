365529b7a6faec06436a071e1f4b2b7b69d5884c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-23 09:58:59 +0530

    Add Product and ProductTab components to enhance product display

--- DELTA/Frontend/REACT/basic-react-app/src/App.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Title, Sum } from "./Title.jsx";
+import ProductTab from "./ProductTab.jsx";
 
 function Description() {
   return <h1>I am Description</h1>;
@@ -8,9 +9,7 @@ function Description() {
 function App() {
   return (
     <>
-      <Title />
-      <Description />
-      <Sum />
+      <ProductTab />
     </>
   );
 }
---
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product() {
+    return (
+        <div>
+            <h1>I am Product</h1>
+            <p>Product details will be here</p>
+        </div>
+    );
+}
+
+export default Product;
