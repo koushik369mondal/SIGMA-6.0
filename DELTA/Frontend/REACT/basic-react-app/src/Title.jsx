5b885dd88315634593389d2653af951fd40e0c41
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-23 09:47:16 +0530

    Convert name to uppercase in Sum function greeting

--- DELTA/Frontend/REACT/basic-react-app/src/Title.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Title.jsx
@@ -8,7 +8,7 @@ function Sum() {
     return (
         <div>
             <p> 2 + 2 = {2+2}</p>
-            <p>Hi, {name}</p>
+            <p>Hi, {name.toUpperCase()}</p>
         </div>
     );
 }
