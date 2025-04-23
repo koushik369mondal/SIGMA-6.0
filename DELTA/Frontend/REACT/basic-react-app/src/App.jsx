5648430f39fed2633d000a20afa1a410428e1607
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-23 09:14:13 +0530

    Add Description component and update App to use it for improved content structure

--- DELTA/Frontend/REACT/basic-react-app/src/App.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/App.jsx
@@ -3,13 +3,17 @@ import "./App.css";
 function Title() {
   return <h1>I am Title</h1>;
 }
+function Description() {
+  return <h1>I am Description</h1>;
+}
 
 function App() {
   return (
     <div>
       <Title />
+      <Description />
       <Title />
-      <Title />
+      <Description />
     </div>
   );
 }
