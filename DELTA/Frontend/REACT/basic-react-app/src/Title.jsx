802bbfc48f67763e7fbc31bebc8c52daae75160b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-23 09:46:27 +0530

    Add name variable to Sum function for personalized greeting

--- DELTA/Frontend/REACT/basic-react-app/src/Title.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Title.jsx
@@ -2,10 +2,13 @@ function Title() {
     return <h1>I am Title</h1>;
 }
 
+let name = "Kaushik Mandal";
+
 function Sum() {
     return (
         <div>
             <p> 2 + 2 = {2+2}</p>
+            <p>Hi, {name}</p>
         </div>
     );
 }
