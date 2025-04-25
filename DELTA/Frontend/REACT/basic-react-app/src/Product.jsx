1e5d439a6552e40034d53670dd9bba0af300969b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 10:45:02 +0530

    Add features prop to Product component and update ProductTab to pass features for phone

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -1,10 +1,12 @@
 import "./Product.css";
 
-function Product({title, price=1}) {
+function Product({title, price=1, features, features2}) {
     return (
         <div className="product">
             <h1>{title}</h1>
             <p>Price: {price}</p>
+            <p>Features:{features}</p>
+            {/* <p>Features:{features2.b}</p> */}
         </div>
     );
 }
--- DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
@@ -1,9 +1,11 @@
 import Product from "./Product";
 
 function ProductTab() {
+    let options = ["hi-tech", "lightweight", "durable"];
+    // let options2 = {a: "hi-tech", b: "lightweight", c: "durable"};
     return (
         <>
-            <Product title="phone"price={40000 / 2} />
+            <Product title="phone"price={40000 / 2} features={options} />
             <Product title="laptop" price={60000}/>
             <Product title="pen"price={10}/>
             <Product title="toffee"/>
