da28a84c864d14eb1910d3ef868fdb1cfc88958b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 23:27:21 +0530

    use && operator

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -5,7 +5,7 @@ function Product({ title, price = 1, features, features2 }) {
         <div className="product">
             <h1>{title}</h1>
             <p>Price: {price}</p>
-            {price > 30000 ? <p>"Discount of 5%"</p> : <a href="/">Get Discount</a>}
+            {price > 30000 && <p>Discount of 5%</p> }
         </div>
     );
 }
