92149160304c9621bab1d66c2f15493c504e0bbd
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 09:21:44 +0530

    Set default price for Product component and add missing price prop for toffee in ProductTab

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -1,6 +1,6 @@
 import "./Product.css";
 
-function Product({title, price}) {
+function Product({title, price=1}) {
     return (
         <div className="product">
             <h1>{title}</h1>
--- DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
@@ -6,6 +6,7 @@ function ProductTab() {
             <Product title="phone"price={40000 / 2} />
             <Product title="laptop" price={60000}/>
             <Product title="pen"price={10}/>
+            <Product title="toffee"/>
         </>
     );
 }
