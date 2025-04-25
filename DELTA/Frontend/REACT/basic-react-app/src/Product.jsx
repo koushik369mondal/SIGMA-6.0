97108e7078ac34f0522ec2b1ccb47ff24a58075c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 23:20:56 +0530

    simplify discount logic and improve readability

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -1,23 +1,15 @@
 import "./Product.css";
 
-function Product({title, price=1, features, features2}) {
-    if(price > 30000) {
-        return (
-            <div className="product">
-                <h1>{title}</h1>
-                <p>Price: {price}</p>
-                <p>Discount of 5%</p>
-            </div>
-        );
-    }
-    else{
-        return (
-            <div className="product">
-                <h1>{title}</h1>
-                <p>Price: {price}</p>
-            </div>
-        );
-    }
+function Product({ title, price = 1, features, features2 }) {
+    let isDiscount = price > 30000 ? "5%" : "";
+
+    return (
+        <div className="product">
+            <h1>{title}</h1>
+            <p>Price: {price}</p>
+            <p>Discount: {isDiscount}</p>
+        </div>
+    );
 }
 
 export default Product;
