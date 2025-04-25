e9a4e0b85a9beca1a4f0309ba752aa82c112bdd3
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 23:24:31 +0530

    Refactor discount display logic for improved clarity

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -1,13 +1,11 @@
 import "./Product.css";
 
 function Product({ title, price = 1, features, features2 }) {
-    let isDiscount = price > 30000 ? "5%" : "";
-
     return (
         <div className="product">
             <h1>{title}</h1>
             <p>Price: {price}</p>
-            <p>Discount: {isDiscount}</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : null}
         </div>
     );
 }
