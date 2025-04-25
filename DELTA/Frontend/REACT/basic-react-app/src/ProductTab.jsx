16ae2c886282347aec2d274ddc912aafbea5cf2d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-25 10:48:19 +0530

    Update ProductTab to use inline features array for phone product

--- DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/ProductTab.jsx
@@ -1,12 +1,12 @@
 import Product from "./Product";
 
 function ProductTab() {
-    let options = ["hi-tech", "lightweight", "durable"];
+    // let options = ["hi-tech", "lightweight", "durable"];
     // let options2 = {a: "hi-tech", b: "lightweight", c: "durable"};
     return (
         <>
-            <Product title="phone"price={40000 / 2} features={options} />
-            <Product title="laptop" price={60000}/>
+            <Product title="phone"price={40000 / 2} features={["hi-tech", "durable"]} />
+            <Product title="laptop" price={60000} />
             <Product title="pen"price={10}/>
             <Product title="toffee"/>
         </>
