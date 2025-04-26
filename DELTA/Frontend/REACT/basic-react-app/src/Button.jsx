a140a7525fd058c3136b917df5b9dc9ac216222c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-26 23:51:03 +0530

    Refactor Button component to improve event handling; change paragraph click to mouse over and ensure double-click functionality is maintained.

--- DELTA/Frontend/REACT/basic-react-app/src/Button.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Button.jsx
@@ -6,11 +6,23 @@ function printBye() {
     console.log("Bye");
 }
 
+function handleDBlclick(){
+    console.log("Double Clicked");
+}
+
 export default function Button() {
-    return(
+    return (
         <div>
             <button onClick={printHello}>Click me</button>
-            <p onClick={printBye}>This is a para</p>
+            <p onMouseOver={printBye}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae
+                ipsa accusantium explicabo, eveniet optio animi. Dicta ut fugit
+                voluptate beatae nesciunt in molestiae facilis reiciendis quos nulla,
+                esse perferendis nisi dolores magnam atque sunt accusantium non enim
+                corrupti! Ad, quasi sequi sint possimus consequatur id quibusdam
+                sapiente facere non optio?
+            </p>
+            <button onDoubleClick={handleDBlclick}>Double Click me</button>
         </div>
-    )
+    );
 }
