8cf62c2a87b87db59d6b49e22ba343fdeae3fbc0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-26 10:21:56 +0530

    add dynamic description

--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -4,10 +4,16 @@ import Price from "./Price";
 function Product({ title, idx}) {
     let oldPrices = ["12,499", "9,999", "1,499", "2,499"];
     let newPrices = ["10,499", "8,999", "1,199", "1,999"];
+    let description = [
+        "Logitech MX Master 3S Wireless Mouse with MagSpeed Scroll Wheel",
+        "Apple Pencil (2nd Generation)",
+        "Zebronics Zeb-Rush Wireless Gaming Mouse with RGB Lights",
+        "Petronix Toad 23 Wireless Gaming Mouse with RGB Lights"
+    ]
     return (
         <div className="product" >
             <h4>{title}</h4>
-            <p>Description</p>
+            <p>{description[idx]}</p>
             <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
