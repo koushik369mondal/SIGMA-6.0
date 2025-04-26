ef2838588eb142b4dedae8aa98cec922cc567e11
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-26 11:04:58 +0530

    Refactor Product component to use nested arrays for descriptions; update Price component styles for better visibility; adjust ProductTab layout in App component.

--- DELTA/Frontend/REACT/basic-react-app/src/App.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/App.jsx
@@ -9,6 +9,7 @@ function Description() {
 function App() {
   return (
     <>
+      <h2>Blockbuster Deals | Shop Now</h2>
       <ProductTab />
     </>
   );
--- DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
+++ DELTA/Frontend/REACT/basic-react-app/src/Product.jsx
@@ -5,15 +5,16 @@ function Product({ title, idx}) {
     let oldPrices = ["12,499", "9,999", "1,499", "2,499"];
     let newPrices = ["10,499", "8,999", "1,199", "1,999"];
     let description = [
-        "Logitech MX Master ",
-        "Apple Pencil (2nd Generation)",
-        "Zebronics Zeb-Rush Wireless ",
-        "Petronix Toad 23 Wireless "
+        ["Logitech MX Master ", "Wireless Mouse"],
+        ["Apple Pencil 2nd gen ", "Stylus Pen"],
+        ["Zebronics Zeb-Rush  ", "Headphones"],
+        ["Petronix Toad 23  ", "Earbuds"],
     ]
     return (
         <div className="product" >
             <h4>{title}</h4>
-            <p>{description[idx]}</p>
+            <p>{description[idx][0]}</p>
+            <p>{description[idx][1]}</p>
             <Price oldPrice={oldPrices[idx]} newPrice={newPrices[idx]}/>
         </div>
     );
