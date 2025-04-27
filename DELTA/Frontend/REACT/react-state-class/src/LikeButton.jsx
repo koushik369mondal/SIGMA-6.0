11dfcfc0e35b645e71c4e41e7bde8b6e0e235971
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-27 11:37:59 +0530

    Add click count display and update toggle functionality in LikeButton component

--- DELTA/Frontend/REACT/react-state-class/src/LikeButton.jsx
+++ DELTA/Frontend/REACT/react-state-class/src/LikeButton.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react';
 
 export default function LikeButton() {
     let [isLiked, setIsLiked] = useState(false);
+    let [clicks, setClicks] = useState(0);
     let toggleLiked = () => {
         setIsLiked(!isLiked);
+        setClicks(clicks + 1);
     }
 
     let likeStyle = {color: "red"};
     
     return (
         <div>
+            <p>Clicks: {clicks}</p>
             <p onClick={toggleLiked}>
                 {
                     isLiked ? (
