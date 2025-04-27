5c0ddcfd86a2643fa21c14b1ac3fac10a4104ea2
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-27 11:35:43 +0530

    Add styling to liked state icon in LikeButton component

--- DELTA/Frontend/REACT/react-state-class/src/LikeButton.jsx
+++ DELTA/Frontend/REACT/react-state-class/src/LikeButton.jsx
@@ -5,13 +5,15 @@ export default function LikeButton() {
     let toggleLiked = () => {
         setIsLiked(!isLiked);
     }
+
+    let likeStyle = {color: "red"};
     
     return (
         <div>
             <p onClick={toggleLiked}>
                 {
                     isLiked ? (
-                        <i className="fa-solid fa-heart"></i>
+                        <i className="fa-solid fa-heart" style={likeStyle}></i>
                     ) : (
                         <i className="fa-regular fa-heart"></i>
                     )
