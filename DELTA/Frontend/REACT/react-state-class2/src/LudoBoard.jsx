01384568ab5ec4bc1df674cb2ffdeeab5325458d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-29 00:17:26 +0530

    Add game board layout to LudoBoard component

--- DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
@@ -2,6 +2,16 @@ export default function LudoBoard() {
     return(
         <div>
             <p>Game Begins</p>
+            <div className="board">
+                <p>Blue moves =</p>
+                <button>+1</button>
+                <p>Yellow moves =</p>
+                <button>+1</button>
+                <p>Green moves =</p>
+                <button>+1</button>
+                <p>Red moves =</p>
+                <button>+1</button>
+            </div>
         </div>
     )
 }
