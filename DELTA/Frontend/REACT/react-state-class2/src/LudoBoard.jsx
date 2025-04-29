d361230e5a3ee2538f55f86f9d41f1a5ef3304e1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-29 08:11:39 +0530

    Add background colors to move buttons in LudoBoard component

--- DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
@@ -7,13 +7,13 @@ export default function LudoBoard() {
             <p>Game Begins</p>
             <div className="board">
                 <p>Blue moves = {moves.blue}</p>
-                <button>+1</button>
+                <button style={{backgroundColor: "blue"}}>+1</button>
                 <p>Yellow moves = {moves.yellow}</p>
-                <button>+1</button>
+                <button style={{backgroundColor: "yellow", color: "black"}}>+1</button>
                 <p>Green moves = {moves.green}</p>
-                <button>+1</button>
+                <button style={{backgroundColor: "green"}}>+1</button>
                 <p>Red moves = {moves.red}</p>
-                <button>+1</button>
+                <button style={{backgroundColor: "red"}}>+1</button>
             </div>
         </div>
     )
