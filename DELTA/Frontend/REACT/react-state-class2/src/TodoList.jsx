5d29ddaf05eff5439d278956513445b39f031511
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-30 00:59:15 +0530

    Add TodoList component to manage tasks in the application

---
+++ DELTA/Frontend/REACT/react-state-class2/src/TodoList.jsx
@@ -0,0 +1,17 @@
+export default function TodoList() {
+    return (
+        <div>
+            <input type="text" placeholder="add a task"/>
+            <br />
+            <button>Add</button>
+            <br />
+            <br />
+            <br />
+            <br />
+            <hr />
+
+            <h4>Tasks Todo</h4>
+            <ul></ul>
+        </div>
+    )
+}
