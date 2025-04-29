7aa277c98f2491f033085d19918ff12bb5a9b6a7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-29 08:05:14 +0530

    Display player move counts in LudoBoard component

--- DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/LudoBoard.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
+
 export default function LudoBoard() {
+    let [blueMove, setBlueMove] = useState(0);
+    let [yellowMove, setYellowMove] = useState(0);
+    let [greenMove, setGreenMove] = useState(0);
+    let [redMove, setRedMove] = useState(0);
     return(
         <div>
             <p>Game Begins</p>
             <div className="board">
-                <p>Blue moves =</p>
+                <p>Blue moves = {blueMove}</p>
                 <button>+1</button>
-                <p>Yellow moves =</p>
+                <p>Yellow moves = {yellowMove}</p>
                 <button>+1</button>
-                <p>Green moves =</p>
+                <p>Green moves = {greenMove}</p>
                 <button>+1</button>
-                <p>Red moves =</p>
+                <p>Red moves = {redMove}</p>
                 <button>+1</button>
             </div>
         </div>
