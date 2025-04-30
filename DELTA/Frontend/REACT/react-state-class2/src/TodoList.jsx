d7e42baa9a63c9319e53f24cad1a70c4d7d4c0fc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-04-30 07:14:31 +0530

    Implement basic functionality for TodoList component to add and display tasks

--- DELTA/Frontend/REACT/react-state-class2/src/TodoList.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/TodoList.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react"
+
 export default function TodoList() {
+    let [todos, setTodos] = useState(["Sample task"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        console.log("We have to add a new task")
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+
     return (
         <div>
-            <input type="text" placeholder="add a task"/>
+            <input type="text" placeholder="add a task" value={newTodo} onChange={updateTodoValue}/>
             <br />
-            <button>Add</button>
+            <button onClick={addNewTask}>Add</button>
             <br />
             <br />
             <br />
@@ -11,7 +24,11 @@ export default function TodoList() {
             <hr />
 
             <h4>Tasks Todo</h4>
-            <ul></ul>
+            <ul>
+                {todos.map((todo) => (
+                    <li>{todo}</li>
+                ))}
+            </ul>
         </div>
     )
 }
