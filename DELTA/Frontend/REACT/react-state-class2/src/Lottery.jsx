8bc6c402e7eeaed7b8f95095b1758f1d4ed54198
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-03 14:17:11 +0530

    Refactor Lottery component to encapsulate ticket buying functionality

--- DELTA/Frontend/REACT/react-state-class2/src/Lottery.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/Lottery.jsx
@@ -5,6 +5,11 @@ import { genTicket, sum } from "./helper";
 export default function Lottery() {
     let [ticket, setTicket] = useState(genTicket(3));
     let isWinning = sum(ticket) === 15;
+
+    let buyTicket = () => {
+        setTicket(genTicket(3));
+    }
+
     return (
         <div>
             <h1>Lottery Game</h1>
@@ -13,6 +18,8 @@ export default function Lottery() {
                 <span>{ticket[1]}</span>
                 <span>{ticket[2]}</span>
             </div>
+            <br />
+            <button onClick={buyTicket}>Buy New Ticket</button>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
     )
