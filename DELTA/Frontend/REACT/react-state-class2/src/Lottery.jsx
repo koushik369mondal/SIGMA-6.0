af6378afcf5aa2adf043b3c7a5a5b39e410490d4
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-04 12:27:38 +0530

    Add Button component and refactor Lottery to use it for ticket purchase

--- DELTA/Frontend/REACT/react-state-class2/src/Lottery.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/Lottery.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { genTicket, sum } from "./helper";
 import Ticket from "./Ticket";
+import Button from "./Button";
 
 export default function Lottery({ n = 3, winCondition }) {
     let [ticket, setTicket] = useState(genTicket(n));
@@ -14,7 +15,7 @@ export default function Lottery({ n = 3, winCondition }) {
         <div>
             <h1>Lottery Game</h1>
             <Ticket ticket={ticket}/>
-            <button onClick={buyTicket}>Buy New Ticket</button>
+            <Button action={buyTicket}/>
             <h3>{isWinning && "Congratulations, you won!"}</h3>
         </div>
     )
