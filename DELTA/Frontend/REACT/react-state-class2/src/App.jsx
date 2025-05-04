90f4eda2c25a26d05b37724998c309ca6b4be9d9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-04 12:18:10 +0530

    Refactor Lottery component to use winCondition prop for determining winning tickets

--- DELTA/Frontend/REACT/react-state-class2/src/App.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
 import Lottery from './Lottery'
 import Ticket from './Ticket'
+import { sum } from './helper'
 
 function App() {
 
+  let winCondition = (ticket) => {
+    return sum(ticket) === 15;
+  }
+
   return (
     <>
-      <Lottery n={3} winningSum={15}/>
+      <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
