4a7cd2a5a39b96137991e73a64e9ce6e44b88427
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-05 01:00:15 +0530

    Refactor Form component to initialize full name state and handle input changes

--- DELTA/Frontend/REACT/react-state-class2/src/components/Form.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Form.jsx
@@ -1,11 +1,21 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export default function Form() {
-    let [fullName, setFullName] = useState("");
+    let [fullName, setFullName] = useState("Kauhsik");
+
+    let handleNameChange = (event) => {
+        setFullName(event.target.value);
+    };
+
     return (
         <form action="">
-            <input type="text" placeholder="Enter your full name" value={fullName}/>
+            <input
+                type="text"
+                placeholder="Enter your full name"
+                value={fullName}
+                onChange={handleNameChange}
+            />
             <button>Submit</button>
         </form>
-    )
+    );
 }
