a225921d1c8ed1af4e540c226da432462c355888
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-06 23:44:17 +0530

    Add username state management and update Form component layout

--- DELTA/Frontend/REACT/react-state-class2/src/components/Form.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Form.jsx
@@ -2,19 +2,34 @@ import { useState } from "react";
 
 export default function Form() {
     let [fullName, setFullName] = useState("");
+    let [username, setUsername] = useState("");
 
     let handleNameChange = (event) => {
         setFullName(event.target.value);
     };
 
+    let handleUsername = (event) => {
+        setUsername(event.target.value);
+    };
+
     return (
         <form action="">
-            <label htmlFor="username">Full Name</label>
+            <label htmlFor="fullName">Full Name</label>
             <input
                 type="text"
                 placeholder="Enter your full name"
                 value={fullName}
                 onChange={handleNameChange}
+                id="fullName"
+            />
+            <br />
+            <br />
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                placeholder="Enter your username"
+                value={username}
+                onChange={handleUsername}
                 id="username"
             />
             <button>Submit</button>
