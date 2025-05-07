3716cfb2d572471a58f1de9ffc93cd874c50177e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-07 19:42:03 +0530

    Refactor Counter component to manage two separate counters and update state handling

--- DELTA/Frontend/REACT/react-state-class2/src/components/Counter.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Counter.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 
 export default function Counter() {
-    let [count, setCount] = useState(0);
+    let [countx, setCountx] = useState(0);
+    let [county, setCounty] = useState(0);
 
-    let incCount = () => {
-        setCount(currCount => currCount + 1);
+    let incCountx = () => {
+        setCountx(currCount => currCount + 1);
+    }
+
+    let incCounty = () => {
+        setCounty(currCount => currCount + 1);
     }
 
     useEffect(function printSomething() {
         console.log("This is a side-effect");
-    })
+    }, [countx]);
 
     return (
         <div>
-            <h3>count = {count}</h3>
-            <button onClick={incCount}>+1</button>
+            <h3>countx = {countx}</h3>
+            <button onClick={incCountx}>+1</button>
+            <h3>county = {county}</h3>
+            <button onClick={incCounty}>+1</button>
         </div>
     );
 }
