0fa2535b0be635f7bb02bc3b54f791eb64b9fd7b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-07 20:16:21 +0530

    Refactor Joker component to streamline joke fetching and improve code structure

--- DELTA/Frontend/REACT/react-state-class2/src/components/Joker.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Joker.jsx
@@ -1,17 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Joker() {
-
     let [joke, setJoke] = useState({});
-
-    const URL = 'https://official-joke-api.appspot.com/random_joke';
-
+    const URL = "https://official-joke-api.appspot.com/random_joke";
     const getNewJoke = async () => {
         let response = await fetch(URL);
         let jsonResponse = await response.json();
-        console.log(jsonResponse);
         setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
-    }
+    };
+
+    useEffect(() => {
+        async function getFirstJoke() {
+            let response = await fetch(URL);
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+        }
+        getFirstJoke();
+    }, []);
 
     return (
         <div>
@@ -20,5 +26,5 @@ export default function Joker() {
             <h2>{joke.punchline}</h2>
             <button onClick={getNewJoke}>Get a new joke</button>
         </div>
-    )
+    );
 }
