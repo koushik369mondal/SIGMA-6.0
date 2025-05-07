4861d373b8b70bc7e50d0a91f878351f656b3c94
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-07 20:07:38 +0530

    Refactor Joker component to improve joke state management and display

--- DELTA/Frontend/REACT/react-state-class2/src/components/Joker.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Joker.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
+
 export default function Joker() {
+
+    let [joke, setJoke] = useState({});
+
     const URL = 'https://official-joke-api.appspot.com/random_joke';
 
     const getNewJoke = async () => {
         let response = await fetch(URL);
         let jsonResponse = await response.json();
         console.log(jsonResponse);
+        setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
     }
 
     return (
         <div>
             <h3>Joker!</h3>
+            <h2>{joke.setup}</h2>
+            <h2>{joke.punchline}</h2>
             <button onClick={getNewJoke}>Get a new joke</button>
         </div>
     )
