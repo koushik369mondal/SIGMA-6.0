9af89929e121cd7be9ea3174bebb88dd6091a875
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-07 16:04:32 +0530

    Refactor Comment and CommentsForm components to improve comment handling and display

--- DELTA/Frontend/REACT/react-state-class2/src/components/Comment.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/components/Comment.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
-import './Comment.css';
+import "./Comment.css";
+import CommentsForm from "./CommentsForm";
 
 export default function Comment() {
-    let [comments, setComments] = useState([{
-        username: "@km",
-        remarks: "Great job!",
-        rating: 5,
-    }]);
+    let [comments, setComments] = useState([
+        {
+            username: "@km",
+            remarks: "Great job!",
+            rating: 5,
+        },
+    ]);
+
+    let addNewComment = (comment) => {
+        setComments((currComments) => [...currComments, comment]);
+        console.log("New comment added");
+    };
+
     return (
-        <div>
-            <h3>All comments</h3>
-            <div className="comment">
-                <span>{comments[0].remarks}</span>
-                &nbsp;
-                <span>(rating ={comments[0].rating})</span>
-                <p>- {comments[0].username}</p>
+        <>
+            <div>
+                <h3>All comments</h3>
+                {comments.map((comment, idx) => (
+                    <div className="comment" key={idx}>
+                        <span>{comment.remarks}</span>
+                        &nbsp;
+                        <span>(rating ={comment.rating})</span>
+                        <p>- {comment.username}</p>
+                    </div>
+                ))}
             </div>
-        </div>
+            <hr />
+            <CommentsForm addNewComment={addNewComment} />
+        </>
     );
 }
