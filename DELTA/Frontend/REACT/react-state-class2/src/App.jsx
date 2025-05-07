55a5935bc506fd410d48ebe453298e4ebd5df5e9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-07 19:28:02 +0530

    Add Counter component to display and increment count

--- DELTA/Frontend/REACT/react-state-class2/src/App.jsx
+++ DELTA/Frontend/REACT/react-state-class2/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Comment from './components/Comment';
 import CommentsForm from './components/CommentsForm';
+import Counter from './components/Counter';
 import Form from './components/Form';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
 
   return (
     <>
-      <Comment />
+      <Counter />
     </>
   )
 }
