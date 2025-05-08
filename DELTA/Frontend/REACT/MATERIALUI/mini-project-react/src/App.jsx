f64346ba847cdf13793f4b1eac53ac0d19e747c6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-09 00:45:08 +0530

    Enhance button functionality and improve code consistency

--- DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/App.jsx
+++ DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/App.jsx
@@ -1,15 +1,32 @@
 // import "./App.css"
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Alert from "@mui/material/Alert";
 
 function App() {
+  let handleClick = () => {
+    console.log("Button clicked");
+  };
   return (
     <>
       <div>
         <h1>Material UI Demo</h1>
-        <Button variant="contained">Click Me</Button>
+        <Button variant="contained" onClick={handleClick}>
+          Click Me
+        </Button>
+
+        <Button variant="contained" onClick={handleClick} color="success" size="small">
+          Click Me2
+        </Button>
+
+        <Button variant="outlined" startIcon={<DeleteIcon />}>
+          Delete
+        </Button>
+
+        <Alert severity="success">This is a success Alert.</Alert>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
