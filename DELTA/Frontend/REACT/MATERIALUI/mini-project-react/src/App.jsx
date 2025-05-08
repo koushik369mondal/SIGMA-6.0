19a7fba72a318060f6c25daa0e575e0698fa88ed
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-09 00:53:51 +0530

    Implement SearchBox component and update App to use it

--- DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/App.jsx
+++ DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/App.jsx
@@ -1,30 +1,11 @@
 // import "./App.css"
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import Alert from "@mui/material/Alert";
+import SearchBox from "./components/SearchBox";
 
 function App() {
-  let handleClick = () => {
-    console.log("Button clicked");
-  };
+
   return (
     <>
-      <div>
-        <h1>Material UI Demo</h1>
-        <Button variant="contained" onClick={handleClick}>
-          Click Me
-        </Button>
-
-        <Button variant="contained" onClick={handleClick} color="success" size="small">
-          Click Me2
-        </Button>
-
-        <Button variant="outlined" startIcon={<DeleteIcon />}>
-          Delete
-        </Button>
-
-        <Alert severity="success">This is a success Alert.</Alert>
-      </div>
+      <SearchBox />
     </>
   );
 }
---
+++ DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/components/SearchBox.jsx
@@ -0,0 +1,7 @@
+export default function SearchBox() {
+    return (
+        <div>
+            <h3>SearchBox</h3>
+        </div>
+    )
+}
