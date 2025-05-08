7538ba32854c7607b7550e225e02a2cdd96b1cd7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-09 01:07:01 +0530

    Fix typo in button label and refactor SearchBox component for improved functionality

--- DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/components/SearchBox.jsx
+++ DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/components/SearchBox.jsx
@@ -1,18 +1,39 @@
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import './SearchBox.css';
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+import { useState } from "react";
 
 export default function SearchBox() {
+    let [city, setCity] = useState("");
+
+    const handleChange = (event) => {
+        setCity(event.target.value);
+    };
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("City name: ", city);
+        setCity("");
+    };
+
     return (
-        <div className='SearchBox'>
+        <div className="SearchBox">
             <h3>Search for the weather</h3>
-            <form action="">
-                <TextField id="city" label="City name" variant="outlined" required />
-                <br /><br />
-                <Button variant="contained" type='submit'>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    id="city"
+                    label="City name"
+                    variant="outlined"
+                    required
+                    value={city}
+                    onChange={handleChange}
+                />
+                <br />
+                <br />
+                <Button variant="contained" type="submit">
                     Seearch
                 </Button>
             </form>
         </div>
-    )
+    );
 }
