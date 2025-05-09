39b4fcdeb257aa322ec4001b45f83b130bee3c3b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-10 01:02:53 +0530

    Add InfoBox component with styling and update SearchBox to include city in weather data

--- DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/components/InfoBox.jsx
+++ DELTA/Frontend/REACT/MATERIALUI/mini-project-react/src/components/InfoBox.jsx
@@ -1,14 +1,14 @@
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
 
 export default function InfoBox() {
     const INIT_URL =
         "https://images.unsplash.com/photo-1564932319746-a617dab0b940?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNsb3VkeSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
     let info = {
+        city: "Guwahati",
         feelsLike: 32.44,
         humidity: 83,
         temp: 27.95,
@@ -17,24 +17,25 @@ export default function InfoBox() {
         weather: "mist",
     };
     return (
-        <div>
+        <div className="InfoBox">
             <h1>Weather Info - {info.weather}</h1>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardMedia sx={{ height: 140 }} image={INIT_URL} title="green iguana" />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
-                </CardActions>
-            </Card>
+            <div className="cardContainer">
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia sx={{ height: 140 }} image={INIT_URL} title="green iguana" />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {info.city}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: "text.secondary" }} component="span">
+                            <p> Temperature = {info.temp}&deg;C</p>
+                            <p>Humidity = {info.humidity}%</p>
+                            <p>Min Temp = {info.tempMin}&deg;C</p>
+                            <p>Max Temp = {info.tempMax}&deg;C</p>
+                            <p>The weather can be described as <i>{info.weather}</i> and feels like = {info.feelsLike}&deg;C</p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
