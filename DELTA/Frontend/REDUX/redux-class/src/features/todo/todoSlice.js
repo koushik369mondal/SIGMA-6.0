45043de425a4083d1ac7c9501da36060ee06bd99
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-12 23:54:27 +0530

    add deleteTodo reducer

--- DELTA/Frontend/REDUX/redux-class/src/features/todo/todoSlice.js
+++ DELTA/Frontend/REDUX/redux-class/src/features/todo/todoSlice.js
@@ -1,20 +1,24 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
     todos: [{ id: "abc", task: "demo-task", isDone: false }],
 };
 
 export const todoSlice = createSlice({
-    name: 'todo',
+    name: "todo",
     initialState,
-    reducers: { //state, action
+    reducers: {
+        //state, action
         addTodo: (state, action) => {
             const newTodo = {
                 id: nanoid(),
                 task: action.payload,
                 isDone: false,
-            }
+            };
             state.todos.push(newTodo); //direct mutation
-        }
-    }
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id != action.payload);
+        },
+    },
 });
