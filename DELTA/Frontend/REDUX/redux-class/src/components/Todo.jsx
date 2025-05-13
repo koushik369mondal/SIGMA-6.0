96f24439c324dc0e5b57d2c3ac8fb14f96121165
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-13 18:37:33 +0530

    add useSelector to fetch todos in Todo component

--- DELTA/Frontend/REDUX/redux-class/src/components/Todo.jsx
+++ DELTA/Frontend/REDUX/redux-class/src/components/Todo.jsx
@@ -1,4 +1,8 @@
+import { useSelector } from "react-redux";
+
 export default function Todo() {
+    const todos = useSelector((state) => state.todos);
+    console.log(todos);
     return (
         <>
             <h2>Todos</h2>
