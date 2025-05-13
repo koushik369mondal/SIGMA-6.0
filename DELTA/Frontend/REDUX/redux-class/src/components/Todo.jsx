ad738c932ce58bdf11bac9ba5969eb54c60253c9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-13 19:57:06 +0530

    add AddForm component and integrate it into Todo component

--- DELTA/Frontend/REDUX/redux-class/src/components/Todo.jsx
+++ DELTA/Frontend/REDUX/redux-class/src/components/Todo.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
+import AddForm from "./AddForm";
 
 export default function Todo() {
     const todos = useSelector((state) => state.todos);
     console.log(todos);
     return (
         <>
-            <h2>Todos</h2>
+            <AddForm />
+            <h2>Todo List App</h2>
             <ul>
                 {todos.map((todo) => (
                     <li key={todo.id}>{todo.task}</li>
