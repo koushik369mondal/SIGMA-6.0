e5cdddbfe50eed33b580574d91db6cceca24b712
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-24 01:08:48 +0530

    print triangle using stars

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,5 +1,10 @@
 public class JavaBasics{
     public static void main(String[] args) {
-        System.out.println("Hello, Guys!");
+        System.out.println("Hello Guys!");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
