c73dc2cf0023b4b49187c6a07a8176721a2435b9
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-25 01:47:39 +0530

    used multiple type conversions

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        char ch = 'a';
-        int num = ch;
-        System.out.println(num);
+        char a = 'a';
+        char b = 'b';
+        System.out.println((int)(a));
+        System.out.println((int)(b));
+        System.out.println(a);
+        System.out.println(b-a);
     }
 }
