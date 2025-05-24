d64342e0c874485dad7290f6476eae28ef642abc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-25 00:49:27 +0530

    convert a character to its integer ASCII value

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float a = 23.12f;
-        int b = (int)a;
-        System.out.println(b);
+        char ch = 'a';
+        int num = ch;
+        System.out.println(num);
     }
 }
