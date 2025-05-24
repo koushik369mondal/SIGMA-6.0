4dcd4aaf0711713af1e1bf77381ddd3943736970
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-24 17:26:15 +0530

    refactor JavaBasics to prompt for user name and greet the user

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,7 +2,11 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        System.out.println("You entered: " + input);
+        // String input = sc.next();
+        // System.out.println("You entered: " + input);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
     }
 }
