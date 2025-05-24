9d2b5fe2fb68a8882c5ac98c9368594edf0d1092
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-24 16:43:00 +0530

    use data types and their values

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,10 +1,14 @@
 public class JavaBasics{
     public static void main(String[] args) {
-        System.out.println("Hello Guys!");
-        System.out.println("*****");
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        byte b = 10;
+        System.out.println("Byte value: " + b);
+        char c = 'A';
+        System.out.println("Character value: " + c);
+        boolean var = true;
+        System.out.println("Boolean value: " + var);
+        float price = 10.5f;
+        System.out.println("Float value: " + price);
+        short s = 1000;
+        System.out.println("Short value: " + s);
     }
 }
