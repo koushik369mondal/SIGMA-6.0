88264ed1400ed54420ff4ea3389da15a994cb866
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-25 00:46:34 +0530

    a float value and convert it to an integer for output

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float rad = sc.nextInt();
-        float area = 3.14f * rad * rad;
-        System.out.println("Area: "+area);
+        float a = 23.12f;
+        int b = (int)a;
+        System.out.println(b);
     }
 }
