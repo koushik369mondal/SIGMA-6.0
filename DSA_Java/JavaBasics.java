0cda0b5c662447bc44a76aab6eeaf6d3696a9013
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-24 17:21:22 +0530

    use user input functionality

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,14 +1,8 @@
+import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
-        byte b = 10;
-        System.out.println("Byte value: " + b);
-        char c = 'A';
-        System.out.println("Character value: " + c);
-        boolean var = true;
-        System.out.println("Boolean value: " + var);
-        float price = 10.5f;
-        System.out.println("Float value: " + price);
-        short s = 1000;
-        System.out.println("Short value: " + s);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println("You entered: " + input);
     }
 }
