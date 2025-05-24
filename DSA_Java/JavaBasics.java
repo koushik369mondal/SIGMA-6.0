fd37809650ab6236a669281c32c6696eb21c7ccc
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-24 17:48:10 +0530

    update JavaBasics to calculate and display the area of a circle based on user input

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,8 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        float b = sc.nextFloat();
-        float product = a * b;
-        System.out.println("Product: " + product);
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println("Area: "+area);
     }
 }
