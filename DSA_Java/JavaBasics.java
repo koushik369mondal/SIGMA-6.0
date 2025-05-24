7953a50892d2b98986ddce6f053ffa1c3032a32a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-25 01:50:30 +0530

    use short, byte, and char for calculations and output the result

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,11 +1,10 @@
 import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
-        char a = 'a';
-        char b = 'b';
-        System.out.println((int)(a));
-        System.out.println((int)(b));
-        System.out.println(a);
-        System.out.println(b-a);
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
+        System.out.println(bt);
     }
 }
