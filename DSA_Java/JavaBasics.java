617fb69ebea566a8cb48235a19b2f52f0ad79dc6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-26 01:06:48 +0530

    update item price calculation

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,13 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter side:");
-        int side = sc.nextInt();
-        int area = side * side;
-        System.out.println("Area of square: " + area);
+        System.out.println("Enter first item price:");
+        float price1 = sc.nextFloat();
+        System.out.println("Enter second item price:");
+        float price2 = sc.nextFloat();
+        System.out.println("Enter third item price:");
+        float price3 = sc.nextFloat();
+        float totalPrice = price1 + price2 + price3;
+        System.out.println("Total price of items with 18% gst: " + totalPrice * 1.18);
     }
 }
