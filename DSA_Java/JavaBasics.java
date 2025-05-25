e623e01995e6fc4c07268e9236eae7159b75e161
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-26 00:49:43 +0530

    simplify calculations using byte type

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,11 +1,8 @@
 import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
-        int a  = 10;
-        float b = 20.25f;
-        long c = 25;
-        double d = 30.5;
-        double ans = a + b + c + d;
-        System.out.println(ans);
+        byte b = 5;
+        byte a = (byte) (b * 2);
+        System.out.println("Value of a: " + a);
     }
 }
