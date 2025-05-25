9e41c542276feecd7a28f4f73cd1d7e6f7d115e3
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-26 01:02:24 +0530

    calculate area of a square instead of average of three numbers

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,13 +2,9 @@ import java.util.*;
 public class JavaBasics{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter second number:");
-        int b = sc.nextInt();
-        System.out.println("Enter third number:");
-        int c = sc.nextInt();
-        int avg = (a + b + c) / 3;
-        System.out.println("The average of the three numbers is: " + avg);
+        System.out.println("Enter side:");
+        int side = sc.nextInt();
+        int area = side * side;
+        System.out.println("Area of square: " + area);
     }
 }
