749927c2645abfe5a585626b3df52a31024451ea
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-27 00:46:41 +0530

    demonstrate increment and decrement operations using unary operators

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,7 +2,23 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        int $ = 24;
-        System.out.println("Value of $: " + $);
+        int a = 10;
+        System.out.println("Initial value of a: " + a);
+
+        // Post-increment: value is used first, then incremented
+        System.out.println("Post-increment (a++): " + a++);
+        System.out.println("Value after post-increment: " + a);
+
+        // Post-decrement: value is used first, then decremented
+        System.out.println("Post-decrement (a--): " + a--);
+        System.out.println("Value after post-decrement: " + a);
+
+        // Pre-decrement: value is decremented first, then used
+        System.out.println("Pre-decrement (--a): " + --a);
+        System.out.println("Value after pre-decrement: " + a);
+
+        // Pre-increment: value is incremented first, then used
+        System.out.println("Pre-increment (++a): " + ++a);
+        System.out.println("Value after pre-increment: " + a);
     }
 }
