4280d2d1d86aceb3b2b38528b38f1e4f71aa509d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 00:17:55 +0530

    implement pass/fail result based on marks

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = 4;
-        String type = (num % 2 == 0) ? "even" : "odd";
-        System.out.println(type);
+        int marks = 67;
+        String result = (marks >= 33) ? "PASS" : "FAIL";
+        System.out.println(result);
     }
 }
