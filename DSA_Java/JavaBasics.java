82e3d490c1c2ce1bed85e6bb2e06cbf3ef7224a1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-30 11:23:42 +0530

    use post-decrement operator

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         int a = 10;
-        int b = ++a;
+        int b = a--;
         System.out.println(a);
         System.out.println(b);
     }
