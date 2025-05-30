7229592eb6f39e272e95a67a1dfc63a15289d36f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-30 18:24:21 +0530

    check for even or odd

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,15 +3,12 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int number1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int number2 = sc.nextInt();
-
-        if (number1 > number2){
-            System.out.println("Number 1 is greater");
-        }else{
-            System.out.println("Number 2 is greater");
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        if( number % 2 == 0){
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
         }
     }
 }
