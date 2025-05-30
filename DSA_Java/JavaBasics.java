a30ba1231c0d9cc37cf2540794e9f89d4124d545
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-30 14:18:54 +0530

    calculate and display expression result

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        int A = 10;
-        // A = A + 10;
-        A += 10;
-        System.out.println("A = " + A);
+        int x  = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+        int exp = 4/3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
+        System.out.println(exp);
     }
 }
