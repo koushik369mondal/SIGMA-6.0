1680bd8230079d880729b65563476f84ec6ae8fa
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-30 11:20:32 +0530

    post increment

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        int _C = 10;
-        int $ = 20;
-        System.out.println("Value of _C is: " + _C);
-        System.out.println("Value of $ is: " + $);
+        int a = 10;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
