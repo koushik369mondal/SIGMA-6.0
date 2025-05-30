6091c0b745da4f97051e7206f5fb16e3d7f5a1e1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-30 10:57:02 +0530

    Add variable declarations and print statements for demonstration

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        
+        int _C = 10;
+        int $ = 20;
+        System.out.println("Value of _C is: " + _C);
+        System.out.println("Value of $ is: " + $);
     }
 }
