9cddfe977f6eb04ed96d277206bf51225fa5397b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 11:52:09 +0530

    leap year check based on user input

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,10 +3,13 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = 63, b = 36;
-        boolean x = (a<b)? true : false;
-        System.out.println(x);
-        int y = (a>b)? a : b;
-        System.out.println(y);
+        System.out.println("Enter a year: ");
+        int year = sc.nextInt();
+        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a leap year");
+        }
     }
 }
