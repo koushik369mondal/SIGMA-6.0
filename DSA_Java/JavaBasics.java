bee3d66a58e20a4d86a6334c829cadda03c43608
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 11:56:50 +0530

    Refactor leap year check to use boolean variables for clarity and improved logic

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -5,11 +5,15 @@ public class JavaBasics {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a year: ");
         int year = sc.nextInt();
-        if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-            System.out.println("Leap year");
-        }
-        else{
-            System.out.println("Not a leap year");
+        
+        boolean x = (year % 4) == 0;
+        boolean y = (year % 100) != 0;
+        boolean z = ((year % 100 == 0) && (year % 400 == 0));
+
+        if(x && (y || z)){
+            System.out.println(year + " is a leap year");
+        } else{
+            System.out.println(year + " is not a leap year");
         }
     }
 }
