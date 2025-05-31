f8d435671c21d9f1a36e8d795c067b53e50a8bb2
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 19:21:22 +0530

    a loop to print "Hello" ten times

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,16 +3,8 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int count = 1;
-        System.out.println("Enter the range: ");
-        int range = sc.nextInt();
-
-        while(count <= range){
-            sum = sum + count;
-            count++;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(" Hello");
         }
-
-        System.out.println("Sum of first " + range + " natural numbers is: " + sum);
     }
 }
