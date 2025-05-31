07c84bb4d7e57b7b68c0e7e3332a3445f818f880
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 14:34:29 +0530

    Add sum calculation for natural numbers up to user-defined range

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,12 +3,16 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int sum = 0;
         int count = 1;
         System.out.println("Enter the range: ");
         int range = sc.nextInt();
+
         while(count <= range){
-            System.out.print(count + " ");
+            sum = sum + count;
             count++;
         }
+
+        System.out.println("Sum of first " + range + " natural numbers is: " + sum);
     }
 }
