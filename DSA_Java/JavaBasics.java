d705a2c0bc6c4d8b6f80d09792dbf336ff8691a3
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 00:18:41 +0530

    check for prime status, removing the do-while loop and enhancing user interaction with clear output messages.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,16 +3,27 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        do{
-            System.out.println("Enter a number: ");
-            int num = sc.nextInt();
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
 
-            if(num % 10 == 0){
-                continue;
+        if (n < 2) {
+            System.out.println("n is not prime");
+        } else if (n == 2) {
+            System.out.println("n is prime");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break; // Exit early if any factor is found
+                }
             }
 
-            System.out.println("Number was: "+ num);
-        }while(true);
+            if (isPrime) {
+                System.out.println("n is prime");
+            } else {
+                System.out.println("n is not prime");
+            }
+        }
     }
 }
