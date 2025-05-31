9bfe070fb06200607b299fdea3ce93f58ed791b4
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 11:33:08 +0530

    selection with boolean and ternary operations for comparison

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,24 +3,10 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter week number: ");
-        int day = sc.nextInt();
-        switch(day){
-            case 1: System.out.println("Sunday");
-            break;
-            case 2: System.out.println("Monday");
-            break;
-            case 3: System.out.println("Tuesday");
-            break;
-            case 4: System.out.println("Wednesday");
-            break;
-            case 5: System.out.println("Thursday");
-            break;
-            case 6: System.out.println("Friday");
-            break;
-            case 7: System.out.println("Saturday");
-            break;
-            default: System.out.println("Invalid day");
-        }
+        int a = 63, b = 36;
+        boolean x = (a<b)? true : false;
+        System.out.println(x);
+        int y = (a>b)? a : b;
+        System.out.println(y);
     }
 }
