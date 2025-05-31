11d56372d27b2c141856a6765214a2912403a780
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 11:17:24 +0530

    check for positive, negative, or zero values instead of arithmetic operations

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,25 +3,14 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int a = sc.nextInt();
-        System.out.println("Enter second number:");
-        int b = sc.nextInt();
-        System.out.println("Enter the operator:");
-        char operator = sc.next().charAt(0);
-        
-        switch(operator){
-            case '+': System.out.println(a+b);
-            break;
-            case '-': System.out.println(a-b);
-            break;
-            case '*': System.out.println(a*b);
-            break;
-            case '/': System.out.println(a/b);
-            break;
-            case '%': System.out.println(a%b);
-            break;
-            default: System.out.println("Invalid operator");
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+        if(num > 0){
+            System.out.println("Positive");
+        }else if(num < 0){
+            System.out.println("Negative");
+        }else{
+            System.out.println("Zero");
         }
     }
 }
