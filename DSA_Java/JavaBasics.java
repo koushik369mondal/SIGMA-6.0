5b9931d66dca19d5a7fbb2bb99d740734304ec60
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 11:23:02 +0530

    check with week day selection using switch-case structure

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,12 +3,24 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double temp = 103.5;
-        if(temp > 100){
-            System.out.println("You have a fever");
-        }
-        else{
-            System.out.println("You don't have a fever");
+        System.out.println("Enter week number: ");
+        int day = sc.nextInt();
+        switch(day){
+            case 1: System.out.println("Sunday");
+            break;
+            case 2: System.out.println("Monday");
+            break;
+            case 3: System.out.println("Tuesday");
+            break;
+            case 4: System.out.println("Wednesday");
+            break;
+            case 5: System.out.println("Thursday");
+            break;
+            case 6: System.out.println("Friday");
+            break;
+            case 7: System.out.println("Saturday");
+            break;
+            default: System.out.println("Invalid day");
         }
     }
 }
