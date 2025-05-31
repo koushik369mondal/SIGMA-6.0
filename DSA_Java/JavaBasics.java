3b2e695dc749dbb3432970c2dad4c0c4f6065197
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 14:24:24 +0530

    Allow user to specify range for counting loop instead of fixed limit

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -4,7 +4,9 @@ public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int count = 1;
-        while(count <= 10){
+        System.out.println("Enter the range: ");
+        int range = sc.nextInt();
+        while(count <= range){
             System.out.print(count + " ");
             count++;
         }
