6c54abb83960abcd1691da9a6c08e1887056361d
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 19:48:01 +0530

    print digits of a number in reverse order

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        int line = 1;
-        while(line <= 4){
-            System.out.println("****");
-            line++;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int lastDigit = 0;
+        while(n > 0){
+            lastDigit = n % 10;
+            System.out.print(lastDigit);
+            n = n / 10;
         }
     }
 }
