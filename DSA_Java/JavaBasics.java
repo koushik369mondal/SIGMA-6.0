74eca5f46eb05db8cf33364bee6a2fcebfd40006
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 19:34:53 +0530

    print asterisks four times

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for(int i = 1; i <= 10; i++){
-            System.out.println(" Hello");
+        for(int i = 1; i <= 4; i++){
+            System.out.println("****");
         }
     }
 }
