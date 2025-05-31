49e8865b49047f4a69ef51c7f166856725a6c918
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 20:17:27 +0530

    number reversal logic with a do-while loop to print "Hello World" multiple times

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        int n = 10899;
-        int reverse = 0;
-        while(n > 0){
-            int lastDigit = n % 10;
-            reverse = (reverse * 10) + lastDigit;
-            n = n / 10;
-        }
-        System.out.println(reverse);
+        int i = -2;
+        do{
+            System.out.println("Hello World");
+            i++;
+        }while(i < 3);
     }
 }
