606de8f64ec11f0f90bb364f548513c074fea7ca
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 14:21:25 +0530

    Refactor counting loop to start from 1 and print numbers up to 10

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = 0;
-        while(count < 10){
-            count = count + 1;
+        int count = 1;
+        while(count <= 10){
             System.out.println(count);
-            
+            count++;
         }
     }
 }
