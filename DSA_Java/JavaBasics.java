3efc209918e88a9ed2bedfac9c6c56f1b3a6f8f4
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 19:38:38 +0530

    Refactor loop to use a while statement for printing asterisks four times

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for(int i = 1; i <= 4; i++){
+        int line = 1;
+        while(line <= 4){
             System.out.println("****");
+            line++;
         }
     }
 }
