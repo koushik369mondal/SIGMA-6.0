c6ce965e628de5878cb05b88e9a651b2f5593896
Kaushik Mandal <koushik369mondal@gmail.com>
2025-05-31 20:12:02 +0530

    Refactor number reversal logic to eliminate user input and directly compute reverse of a fixed number

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,15 +2,13 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
-        int n = sc.nextInt();
-        int lastDigit = 0;
+        int n = 10899;
+        int reverse = 0;
         while(n > 0){
-            lastDigit = n % 10;
-            System.out.print(lastDigit);
-            n /= 10;
+            int lastDigit = n % 10;
+            reverse = (reverse * 10) + lastDigit;
+            n = n / 10;
         }
-        System.out.println();
+        System.out.println(reverse);
     }
 }
