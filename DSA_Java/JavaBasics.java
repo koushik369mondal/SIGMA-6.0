902858aeef484310e43c9a5b0a95d550a0bec687
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 19:20:14 +0530

    Implement user input for summing even and odd integers

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,26 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            System.out.println("Hello");
-            i += 2;
+        Scanner sc = new Scanner(System.in);
+
+        int evenSum = 0;
+        int oddSum = 0;
+        System.out.println("Enter a number of integers: ");
+        int count = sc.nextInt();
+
+        System.out.println("Enter "+ count + " integers");
+
+        for (int i = 0; i < count; i++) {
+            int n = sc.nextInt();
+
+            if (n % 2 == 0) {
+                evenSum += n;
+            } else {
+                oddSum += n;
+            }
         }
+
+        System.out.println("Even Sum = " + evenSum);
+        System.out.println("Odd Sum = " + oddSum);
     }
 }
