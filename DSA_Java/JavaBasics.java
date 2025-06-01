98c0aabdeb6a54cd9baf4e181784db4e2c90087e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 20:32:44 +0530

    first and send in one

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,6 +2,13 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
+
+        System.out.println("First");
+        for (int idx = 2; idx % 3 != 0 && idx % 5 != 0; idx++) {
+            System.out.println(idx);
+        }
+
+        System.out.println("Second");
         for (int idx = 2; idx % 3 != 0 || idx % 5 != 0; idx++) {
             System.out.println(idx);
         }
