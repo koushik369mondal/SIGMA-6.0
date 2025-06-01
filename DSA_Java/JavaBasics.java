1cc25389a06b2c0db401041cfeb77d1e3d563250
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-02 00:08:13 +0530

    print a pyramid pattern

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,25 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int ctr3 = 0, ctr5 = 0;
-
-        for (int i = 1; i <= N; i++) {
-            ctr3++;
-            ctr5++;
-            if (ctr3 == 3 && ctr5 == 5) {
-                System.out.print("FizzBuzz ");
-                ctr3 = ctr5 = 0;
-            } else if (ctr3 == 3) {
-                System.out.print("Fizz ");
-                ctr3 = 0;
-            } else if (ctr5 == 5) {
-                System.out.print("Buzz ");
-                ctr5 = 0;
-            } else {
-                System.out.print(i + " ");
+        for(int line=1; line<=4; line++){
+            for(int star=1; star<=line; star++){
+                System.out.print("*");
             }
+            System.out.println();
         }
     }
 }
