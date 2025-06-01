401a7d39119cfbdd386dca3c4a9ffa95f9684604
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 23:08:06 +0530

    Refactor Fizz Buzz implementation to use counters for 3 and 5, and adjust loop to include the upper limit input by the user.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -4,17 +4,22 @@ public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        int n = sc.nextInt();
+        int N = sc.nextInt();
+        int ctr3 = 0, ctr5 = 0;
 
-        for (int i = 1; i < n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("Fizz Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+        for (int idx = 1; idx <= N; idx++) {
+            ctr3++;
+            ctr5++;
+
+            if (ctr3 != 3 && ctr5 != 5) {
+                System.out.print(idx + " ");
+            } else if (ctr3 == 3) {
+                System.out.print(" Fizz ");
+                ctr3 = 0;
+            }
+            if (ctr5 == 5) {
+                System.out.print(" Buzz ");
+                ctr5 = 0;
             }
         }
     }
