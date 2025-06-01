25c95131dbb80988839ca1a2d30604d6a659cd14
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 22:25:57 +0530

    implement Fizz Buzz logic and add user input for range;

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,15 +2,20 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
 
-        System.out.println("First");
-        for (int idx = 2; idx % 3 != 0 && idx % 5 != 0; idx++) {
-            System.out.println(idx);
-        }
-
-        System.out.println("Second");
-        for (int idx = 2; idx % 3 != 0 || idx % 5 != 0; idx++) {
-            System.out.println(idx);
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else{
+                System.out.println(i);
+            }
         }
     }
 }
