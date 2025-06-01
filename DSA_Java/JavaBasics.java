bdf255a4fdf013f78099640d92a99a6cef10be4f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 19:26:20 +0530

    calculate factorial of a user-input number

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -4,24 +4,13 @@ public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int evenSum = 0;
-        int oddSum = 0;
-        System.out.println("Enter a number of integers: ");
-        int count = sc.nextInt();
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int fact = 1;
 
-        System.out.println("Enter "+ count + " integers");
-
-        for (int i = 0; i < count; i++) {
-            int n = sc.nextInt();
-
-            if (n % 2 == 0) {
-                evenSum += n;
-            } else {
-                oddSum += n;
-            }
+        for(int i=num; i>1; i-- ){
+            fact *= i;
         }
-
-        System.out.println("Even Sum = " + evenSum);
-        System.out.println("Odd Sum = " + oddSum);
+        System.out.println(fact);
     }
 }
