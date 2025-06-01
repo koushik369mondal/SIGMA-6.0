9c4caf0513a3fb97c6ac2446f66194d7889a655f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 20:29:15 +0530

    Fix loop condition to correctly print numbers not divisible by 3 or 5

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,8 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        for (int idx = 2; idx % 3 != 0 && idx % 5 != 0; idx++) {
+        for (int idx = 2; idx % 3 != 0 || idx % 5 != 0; idx++) {
             System.out.println(idx);
         }
-
     }
 }
