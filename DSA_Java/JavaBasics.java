7b0d8904dc7c8665db381f721cb7a47eda6fd454
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 23:12:47 +0530

    Refactor FizzBuzz logic to correctly print numbers, Fizz, and Buzz based on counters, and improve variable naming for clarity.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -3,23 +3,23 @@ import java.util.*;
 public class JavaBasics {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number: ");
         int N = sc.nextInt();
         int ctr3 = 0, ctr5 = 0;
 
-        for (int idx = 1; idx <= N; idx++) {
+        for (int i = 1; i <= N; i++) {
             ctr3++;
             ctr5++;
-
-            if (ctr3 != 3 && ctr5 != 5) {
-                System.out.print(idx + " ");
+            if (ctr3 == 3 && ctr5 == 5) {
+                System.out.print("FizzBuzz ");
+                ctr3 = ctr5 = 0;
             } else if (ctr3 == 3) {
-                System.out.print(" Fizz ");
+                System.out.print("Fizz ");
                 ctr3 = 0;
-            }
-            if (ctr5 == 5) {
-                System.out.print(" Buzz ");
+            } else if (ctr5 == 5) {
+                System.out.print("Buzz ");
                 ctr5 = 0;
+            } else {
+                System.out.print(i + " ");
             }
         }
     }
