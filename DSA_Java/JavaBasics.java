bc7407e3f6e7edad2d912284a0239534071acaa6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-01 08:14:53 +0530

    implement a simple swap algorithm for two integers without using temp variable

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,28 +2,12 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        if (n < 2) {
-            System.out.println("n is not prime");
-        } else if (n == 2) {
-            System.out.println("n is prime");
-        } else {
-            boolean isPrime = true;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    isPrime = false;
-                    break; // No need to check further
-                }
-            }
-
-            if (isPrime) {
-                System.out.println("n is prime");
-            } else {
-                System.out.println("n is not prime");
-            }
-        }
+        int a = 10;
+        int b = 5;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
