df15c1c976689748b384f2edf7e87a9389bece92
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-02 19:59:22 +0530

    Update workspace configuration and modify JavaBasics to print a sequence of numbers instead of an inverted pyramid pattern.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        for (int line = 1; line <= 5; line++) {
-            for (int star = 5; star >= line; star--) {
-                System.out.print("*");
+        for (int line = 1; line <= 4; line++) {
+            for (int num = 1; num <= line; num++) {
+                System.out.print(num);
             }
             System.out.println();
         }
