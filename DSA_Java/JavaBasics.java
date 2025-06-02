28a6fd68a3e37348aade6474eda06bcb14d953b8
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-02 19:53:50 +0530

    Update workspace configuration and modify JavaBasics to print an inverted pyramid pattern with 5 lines.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        for(int line=1; line<=4; line++){
-            for(int star=1; star<=line; star++){
+        for (int line = 1; line <= 5; line++) {
+            for (int star = 5; star >= line; star--) {
                 System.out.print("*");
             }
             System.out.println();
