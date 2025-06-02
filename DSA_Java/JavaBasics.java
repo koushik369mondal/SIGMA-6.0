da16a6a47a42b5193084f4aca421b1bcb07a6fbd
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-02 23:28:05 +0530

    print a pyramid pattern using characters instead of numbers. Also, add a change entry for a new document in the workspace settings.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -2,9 +2,13 @@ import java.util.*;
 
 public class JavaBasics {
     public static void main(String[] args) {
-        for (int line = 1; line <= 4; line++) {
-            for (int num = 1; num <= line; num++) {
-                System.out.print(num);
+        int n = 4;
+        char ch = 'A';
+
+        for(int line = 1; line <= n; line++){
+            for(int chars=1; chars<=line; chars++){
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
