d863e94986853fe79ee8328788205b724f5d10ef
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-03 10:25:41 +0530

    print "Hello World" using function

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,21 +1,11 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number of rows: ");
-        int n = sc.nextInt();
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
 
-        for (int i = 1; i <= n; i++) {
-            // Print spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // Print stars
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+    public static void main(String[] args) {
+        printHelloWorld();
     }
 }
