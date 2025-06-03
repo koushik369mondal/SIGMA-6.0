af244d4abf82678856e1cc78df1eaa76d14ef56b
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-03 23:06:15 +0530

    Refactor calculateSum method in JavaBasics to accept parameters for input values, enhancing flexibility and usability.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -8,8 +8,8 @@ public class JavaBasics {
         return 3;
     }
 
-    public static void calculateSum(int a, int b) {
-        int sum = a + b;
+    public static void calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
         System.out.println("Sum is : " + sum);
     }
 
