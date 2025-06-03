2e08ffdd7f5cd9a61c9a0611517a9ea5814200cf
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-03 22:39:08 +0530

    Implement calculateSum method in JavaBasics to read two integers from user input and print their sum

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -8,7 +8,15 @@ public class JavaBasics {
         return 3;
     }
 
+    public static void calculateSum() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum is : " + sum);
+    }
+
     public static void main(String[] args) {
-        printHelloWorld();
+        calculateSum();
     }
 }
