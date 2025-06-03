fe9fc6da2a73d01cd254b325c83a4d881d8669aa
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-04 01:14:56 +0530

    swap values of two integers and update workspace settings to track changes in JavaBasics and DSA Notes document.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -7,10 +7,14 @@ public class JavaBasics {
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = calculateSum(a,b); // arguments or actual parameters
-        System.out.println("Sum is : " + sum);
+        int a = 5;
+        int b = 10;
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
