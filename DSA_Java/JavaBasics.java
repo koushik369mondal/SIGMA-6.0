a0130d9c6226e165e249109b03653356ebfff9d7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-03 10:28:06 +0530

    Update printHelloWorld method to return an integer and print "Hello World" multiple times.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,8 +1,11 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void printHelloWorld() {
+    public static int printHelloWorld() {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        return 3;
     }
 
     public static void main(String[] args) {
