757d6b0dbdc4feded32d6bff9e6d546649ef52fa
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-04 19:44:30 +0530

    implement a multiply method, and update DSA Notes with explanations on call by value and call by reference in Java.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,23 +1,14 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static int calculateSum(int num1, int num2) { //parameters or formal parameters
-        int sum = num1 + num2;
-        return sum;
+    public static int multiply(int a, int b){
+        int product = a * b;
+        return product;
     }
-
-    public static void swap(int a, int b){
-        int temp = a;
-        a = b;
-        b = temp;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-    }
-
     public static void main(String[] args) {
         int a = 5;
         int b = 10;
-        swap(a, b);
+        int prod = multiply(a, b);
+        System.out.println("a * b = " + prod);
     }
 }
