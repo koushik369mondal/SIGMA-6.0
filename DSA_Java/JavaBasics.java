aa63cacd170add00caed7df27cd559aa363e211f
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 20:39:37 +0530

    Add decToBin method to JavaBasics for converting decimal to binary and update main method to test with a binary input.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -17,7 +17,22 @@ public class JavaBasics {
         System.out.println("Decimal of " + myNum + " is " + decNum);
     }
 
+    public static void decToBin(int n) {
+        int myNum = n;
+        int pow = 0;
+        int binNum = 0;
+
+        while (n > 0) {
+            int rem = n % 2;
+            binNum = binNum + (rem * (int) Math.pow(10, pow));
+
+            pow++;
+            n = n / 2;
+        }
+        System.out.println("binary form of " + myNum + " is " + binNum);
+    }
+
     public static void main(String[] args) {
-        binToDec(11);
+        binToDec(110101);
     }
 }
