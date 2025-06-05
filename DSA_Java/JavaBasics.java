3422c954fc7c0d6d68e34285f7e1048e3be3c147
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 09:27:11 +0530

    Overloading using Data Types

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -5,12 +5,12 @@ public class JavaBasics {
         return a + b;
     }
 
-    public static int sum(int a, int b, int c){
-        return a + b + c;
+    public static float sum(float a, float b){
+        return a + b ;
     }
 
     public static void main(String[] args) {
         System.out.println(sum(5,2 ));
-        System.out.println(sum(2,3,4));
+        System.out.println(sum(2.3f,3.4f));
     }
 }
