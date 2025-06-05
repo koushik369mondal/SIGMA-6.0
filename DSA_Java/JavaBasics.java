b93293dc594ebb526b2395c8c74a86b215168d4a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 09:21:32 +0530

    Function Overloading using Parameters

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,25 +1,16 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static int factorial(int n){
-        int f = 1;
-
-        for(int i = 1; i <= n; i++){
-            f = f * i;
-        }
-        return f;
+    public static int sum(int a, int b){
+        return a + b;
     }
 
-    public static int binCoeff(int n, int r){
-        int fact_n = factorial(n);
-        int fact_r = factorial(r);
-        int fact_nmr = factorial(n-r);
-
-        int binCoeff = fact_n / (fact_r * fact_nmr);
-        return binCoeff;
+    public static int sum(int a, int b, int c){
+        return a + b + c;
     }
 
     public static void main(String[] args) {
-        System.out.println(binCoeff(5,2 ));
+        System.out.println(sum(5,2 ));
+        System.out.println(sum(2,3,4));
     }
 }
