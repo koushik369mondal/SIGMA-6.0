cba9059c4968760911c91ba1fa787d2d0eb7ba13
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 10:05:22 +0530

    Refactor isPrime method

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,26 +1,16 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static int sum(int a, int b){
-        return a + b;
-    }
-
-    public static float sum(float a, float b){
-        return a + b ;
-    }
-
-    public static boolean isPrime(int n){
-        boolean isPrime = true;
-        for(int i=2; i<=n-1; i++){
-            if(n % i == 0){
-                isPrime = false;
-                break;
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(5));
+        System.out.println(isPrime(50));
     }
 }
