e1b3f422303d2c22bc0cef794d702cccb9ca4b9a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 18:27:24 +0530

    Code - Binary to Decimal

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,28 +1,23 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static boolean isPrime(int n){
-        if(n == 2){
-            return true;
-        }
 
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
-    }
+    public static void binToDec(int binNum) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
 
-    public static void primeInRange(int n){
-        for(int i=2; i<=n; i++){
-            if(isPrime(i)){
-                System.out.print(i+ " ");
-            }
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int) Math.pow(2, pow));
+
+            pow++;
+            binNum = binNum / 10;
         }
+        System.out.println("Decimal of " + myNum + " is " + decNum);
     }
 
     public static void main(String[] args) {
-        primeInRange(20);
+        binToDec(11);
     }
 }
