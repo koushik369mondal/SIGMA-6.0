d261454c3a1d775cca41f6428bdbaeda20e42e4c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-06 00:03:21 +0530

    isEven to check if a number is even or odd, and update main method for user input accordingly.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void myMethod(int a, int b, int c) {
-        int avg = (a + b + c) / 3;
-        System.out.println("AVerage is : " + avg);
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number: ");
-        int a = sc.nextInt();
-        System.out.println("Enter second number: ");
-        int b = sc.nextInt();
-        System.out.println("Enter third number: ");
-        int c = sc.nextInt();
-        myMethod(a, b, c);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isEven(num)) {
+            System.out.println(num + " is even.");
+        } else {
+            System.out.println(num + " is odd.");
+        }
     }
 }
