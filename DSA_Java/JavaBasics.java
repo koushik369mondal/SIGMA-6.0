caac7b642b6996f764ac0c90304b3a124aba7874
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 11:28:00 +0530

    Add primeInRange method to JavaBasics for printing prime numbers up to a given limit and update main method accordingly. Enhance DSA Notes with new content on checking prime numbers in a range.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -14,7 +14,15 @@ public class JavaBasics {
         return true;
     }
 
+    public static void primeInRange(int n){
+        for(int i=2; i<=n; i++){
+            if(isPrime(i)){
+                System.out.print(i+ " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(isPrime(4));
+        primeInRange(20);
     }
 }
