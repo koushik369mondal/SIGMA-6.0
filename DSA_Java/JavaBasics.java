43de220bb56f597cbc77fda5f10a8a3aa49a2125
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-05 23:57:31 +0530

    Refactor myMethod to calculate the average of three numbers and update main method to call it with user input.

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,16 +1,19 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void myMethod() {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        int avg = (a+b+c)/3;
-        System.out.println(avg);
+    public static void myMethod(int a, int b, int c) {
+        int avg = (a + b + c) / 3;
+        System.out.println("AVerage is : " + avg);
     }
 
     public static void main(String[] args) {
-        myMethod();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter third number: ");
+        int c = sc.nextInt();
+        myMethod(a, b, c);
     }
 }
