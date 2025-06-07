74190091d5d0e68dcd351db0ff3cbff85356d296
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 23:26:04 +0530

    Remove Hello class and update hello method in JavaBasics to print a pattern of asterisks

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 public class JavaBasics {
     public static void hello() {
-        System.out.println("Hello");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
