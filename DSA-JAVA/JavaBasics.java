854d6f1c2998b27e436350b9fd557c7de96fd171
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-08 00:25:02 +0530

    Fix condition in pattern method of JavaBasics to correctly print asterisks, ensuring proper grid output formatting.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -6,10 +6,10 @@ public class JavaBasics {
         int m = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("* ");
+                if (i == 1 && i == n && j == 1 && j == n) {
+                    System.out.print(" ");
                 } else {
-                    System.out.print("   ");
+                    System.out.print("* ");
                 }
             }
             System.out.println();
