cabf43d2c50fad7d0d6a0b6bf3d99ff0bc077838
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-08 00:33:33 +0530

    print a right-angled triangle of asterisks, adjusting loop structure for dynamic row output.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,17 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void pattern() {
-        int n = 4;
-        int m = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (i == 1 && i == n && j == 1 && j == n) {
-                    System.out.print(" ");
-                } else {
-                    System.out.print("* ");
-                }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 
