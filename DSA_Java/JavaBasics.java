13e41a613233d2d45620b5ede9d7ae40e18eedf1
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 11:41:59 +0530

    Remove sumOfDigits method and related user input from JavaBasics; add .gitignore to exclude build artifacts

--- DSA_Java/JavaBasics.java
+++ DSA_Java/JavaBasics.java
@@ -1,28 +1,11 @@
 import java.util.*;
 
 public class JavaBasics {
-
-    // Method to calculate sum of digits
-    public static int sumOfDigits(int num) {
-        int sum = 0;
-
-        while (num > 0) {
-            int lastDigit = num % 10;  // b. Find last digit
-            sum += lastDigit;          // c. Add to sum
-            num /= 10;                 // d. Remove last digit
-        }
-
-        return sum;
+    public static void hello() {
+        System.out.println("Hello");
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-
-        int result = sumOfDigits(number);  // Call the method
-
-        System.out.println("Sum of digits: " + result);
+        hello();
     }
 }
