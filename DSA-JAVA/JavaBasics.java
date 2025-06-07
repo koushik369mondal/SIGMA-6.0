3261f1d652c175a07c11ef5edf1c1e6e75fc8027
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-08 00:22:37 +0530

    Update pattern method in JavaBasics to change grid size from 5 to 4 and adjust inner spacing for improved output formatting

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class JavaBasics {
     public static void pattern() {
-        int n = 5;
+        int n = 4;
         int m = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("* ");
                 } else {
-                    System.out.print("  ");
+                    System.out.print("   ");
                 }
             }
             System.out.println();
