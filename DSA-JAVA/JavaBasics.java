3724bf4e6ae39719861cc1412618c26517ff57cf
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 23:47:59 +0530

    print a grid of asterisks with updated loop indices

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void hello() {
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(i);
-                System.out.println(j);
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
             }
+            System.out.println();
         }
     }
 
