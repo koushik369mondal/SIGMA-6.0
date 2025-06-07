fb8e81074e8eb6e7fa8f9dee916083d1e283019a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 12:11:48 +0530

    Reintroduce JavaBasics class with hello method and main execution

---
+++ DSA-JAVA/JavaBasics.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaBasics {
+    public static void hello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        hello();
+    }
+}
