95f036dd9d23e21d97e99360dc7ffeb9bc1bf327
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-08 00:17:57 +0530

    Update pattern method in JavaBasics to print a border of asterisks for a specified grid size

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,12 +2,16 @@ import java.util.*;
 
 public class JavaBasics {
     public static void pattern() {
-        // for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 4; j++) {
-                System.out.print("* ");
+        int n = 5;
+        int m = 6;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n) {
+                    System.out.print("* ");
+                }
             }
-            // System.out.println();
-        // }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
