09efb8c57b8746f1adf88e4ef91c33a08ea998ac
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 23:45:16 +0530

    Update hello method in JavaBasics to print indices instead of asterisks

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void hello() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i);
+                System.out.println(j);
             }
-            System.out.println();
         }
     }
 
