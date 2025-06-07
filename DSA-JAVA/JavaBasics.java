e43c11c83135e50f2b66a98c84b4f8d1cf1eeeef
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-07 23:50:23 +0530

    Refactor JavaBasics class to rename hello method to pattern and modify its functionality to print a grid of asterisks

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void hello() {
-        for (int i = 1; i <= 3; i++) {
+    public static void pattern() {
+        // for (int i = 1; i <= 3; i++) {
             for (int j = 1; j <= 4; j++) {
                 System.out.print("* ");
             }
-            System.out.println();
-        }
+            // System.out.println();
+        // }
     }
 
     public static void main(String[] args) {
-        hello();
+        pattern();
     }
 }
