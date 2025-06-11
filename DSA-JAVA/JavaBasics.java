74b7bdf392346f7b01cb12fbfefddb266a856b13
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-12 02:12:46 +0530

    implement a 0-1 triangle pattern and update DSA Notes to include a section for the new pattern.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -5,8 +5,11 @@ public class JavaBasics {
         int counter = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(counter + " ");
-                counter++;
+                if ((i + j) % 2 == 0) { 
+                    System.out.print("1 "); //even
+                } else { 
+                    System.out.print("0 "); //odd
+                }
             }
             System.out.println();
         }
