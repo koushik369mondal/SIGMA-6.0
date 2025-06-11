37abd84d77364650b02ac767020acdb899161d4e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-12 00:32:44 +0530

    print an inverted half pyramid of numbers, enhancing output structure. Update DSA Notes to include a section for the new pattern.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -3,13 +3,8 @@ import java.util.*;
 public class JavaBasics {
     public static void pattern(int n) {
         for (int i = 1; i <= n; i++) {
-            //space
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print("  ");
-            }
-            //stars
-            for(int j=1; j<=i; j++){
-                System.out.print(" *");
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
@@ -17,6 +12,5 @@ public class JavaBasics {
 
     public static void main(String[] args) {
         pattern(5);
-        ;
     }
 }
