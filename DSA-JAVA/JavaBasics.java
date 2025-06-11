a9a0b5ecadc8c2612b5906606849800ebbf3fbcf
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-12 02:27:58 +0530

    Remove unused counter variable from pattern method in JavaBasics.java to improve code clarity.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class JavaBasics {
     public static void pattern(int n) {
-        int counter = 1;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 if ((i + j) % 2 == 0) { 
