ca942cee2ae1d93ab617aba9594a5651469b8966
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-12 01:57:49 +0530

    Implement Floyd's Triangle pattern in JavaBasics.java and update DSA Notes to include a section for the new pattern.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class JavaBasics {
     public static void pattern(int n) {
+        int counter = 1;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print(j + " ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
             }
             System.out.println();
         }
