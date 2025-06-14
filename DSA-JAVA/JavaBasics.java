e8a26baca4820f267094232b4269f904464a11a7
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-14 17:18:39 +0530

    print butterfly pattern

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -1,20 +1,47 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void pattern(int n) {
+    public static void butterfly(int n) {
+        // 1st half
         for (int i = 1; i <= n; i++) {
+            // stars - i
             for (int j = 1; j <= i; j++) {
-                if ((i + j) % 2 == 0) { 
-                    System.out.print("1 "); //even
-                } else { 
-                    System.out.print("0 "); //odd
-                }
+                System.out.print("* ");
+            }
+
+            // spaces - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // 2nd half
+        for (int i = n; i >= 1; i--) {
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // spaces - 2*(n-i)
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+
+            // stars - i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        pattern(5);
+        butterfly(4);
     }
 }
