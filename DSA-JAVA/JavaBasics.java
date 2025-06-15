5bb1bbc8b2625fde9fa3a2a4b757d293539f316e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-15 10:42:35 +0530

    implement hollow_rhombus method

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -36,15 +36,33 @@ public class JavaBasics {
     // }
     // }
 
-    public static void solid_rhombus(int n) {
+    // public static void solid_rhombus(int n) {
+    // for (int i = 1; i <= n; i++) {
+    // // spaces
+    // for (int j = 1; j <= (n - i); j++) {
+    // System.out.print(" ");
+    // }
+    // //stars
+    // for(int j=1; j<=n; j++){
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    public static void hollow_rhombus(int n) {
         for (int i = 1; i <= n; i++) {
             // spaces
             for (int j = 1; j <= (n - i); j++) {
                 System.out.print("  ");
             }
-            //stars
-            for(int j=1; j<=n; j++){
-                System.out.print("* ");
+            // hollow rectangle - stars
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
             System.out.println();
         }
@@ -52,6 +70,7 @@ public class JavaBasics {
 
     public static void main(String[] args) {
         // butterfly(4);
-        solid_rhombus(5);
+        // solid_rhombus(5);
+        hollow_rhombus(5);
     }
 }
