b0dad7103b531c66ebc3e23be62cc6da3ea996ab
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-15 09:14:45 +0530

    implement solid rhombus pattern. Update main method to call solid_rhombus(5) for demonstration.

--- DSA-JAVA/JavaBasics.java
+++ DSA-JAVA/JavaBasics.java
@@ -1,40 +1,49 @@
 import java.util.*;
 
 public class JavaBasics {
-    public static void butterfly(int n) {
-        // 1st half
+    // public static void butterfly(int n) {
+    // // 1st half
+    // for (int i = 1; i <= n; i++) {
+    // // stars - i
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("* ");
+    // }
+    // // spaces - 2*(n-i)
+    // for (int j = 1; j <= 2 * (n - i); j++) {
+    // System.out.print(" ");
+    // }
+    // // stars - i
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+    // // 2nd half
+    // for (int i = n; i >= 1; i--) {
+    // // stars - i
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("* ");
+    // }
+    // // spaces - 2*(n-i)
+    // for (int j = 1; j <= 2 * (n - i); j++) {
+    // System.out.print(" ");
+    // }
+    // // stars - i
+    // for (int j = 1; j <= i; j++) {
+    // System.out.print("* ");
+    // }
+    // System.out.println();
+    // }
+    // }
+
+    public static void solid_rhombus(int n) {
         for (int i = 1; i <= n; i++) {
-            // stars - i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-
-            // spaces - 2*(n-i)
-            for (int j = 1; j <= 2 * (n - i); j++) {
+            // spaces
+            for (int j = 1; j <= (n - i); j++) {
                 System.out.print("  ");
             }
-
-            // stars - i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-
-        // 2nd half
-        for (int i = n; i >= 1; i--) {
-            // stars - i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
-
-            // spaces - 2*(n-i)
-            for (int j = 1; j <= 2 * (n - i); j++) {
-                System.out.print("  ");
-            }
-
-            // stars - i
-            for (int j = 1; j <= i; j++) {
+            //stars
+            for(int j=1; j<=n; j++){
                 System.out.print("* ");
             }
             System.out.println();
@@ -42,6 +51,7 @@ public class JavaBasics {
     }
 
     public static void main(String[] args) {
-        butterfly(4);
+        // butterfly(4);
+        solid_rhombus(5);
     }
 }
