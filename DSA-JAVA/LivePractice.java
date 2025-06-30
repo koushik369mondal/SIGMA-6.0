234d904f5af9287cf938aed6b147d897879a6610
Kaushik Mandal <koushik369mondal@gmail.com>
2025-06-30 21:53:43 +0530

    binaryToDecimal for converting binary numbers to decimal.

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -2,21 +2,26 @@ import java.util.*;
 
 class LivePractice {
 
-    static int sumOfDigits(int n) {
+    static int binaryToDecimal(int n) {
+        int mul = 1;
         int sum = 0;
         while (n > 0) {
             int digit = n % 10;
-            sum = sum + digit;
+            sum = sum + digit * mul;
             n = n / 10;
+            mul = mul * 2;
         }
         return sum;
     }
 
     public static void main(String[] args) {
-        int result1 = sumOfDigits(135);
-        System.out.println("135 sum of digits: " + result1);
+        int result1 = binaryToDecimal(101);
+        System.out.println("101 : " + result1);
 
-        int result2 = sumOfDigits(279);
-        System.out.println("279 sum of digits: " + result2);
+        int result2 = binaryToDecimal(111);
+        System.out.println("111 : " + result2);
+
+        int result3 = binaryToDecimal(1000);
+        System.out.println("1000 : " + result3);
     }
 }
