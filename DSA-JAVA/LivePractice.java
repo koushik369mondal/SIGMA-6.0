879a2cef3402e24da4f851febaaf4fa99c506fa6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-01 16:34:28 +0530

    palindromic pattern

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -2,28 +2,6 @@ import java.util.*;
 
 class LivePractice {
     public static void main(String[] args) {
-        int n = 3;
-        for (int i = 1; i <= n; i++) {
-            int stars = 2 * i - 1;
-            int spaces = n - i;
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print("   ");
-            }
-            for (int j = 1; j <= stars; j++) {
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
-        for (int i = n; i >= 1; i--) {
-            int stars = 2 * i - 1;
-            int spaces = n - i;
-            for (int j = 1; j <= spaces; j++) {
-                System.out.print("   ");
-            }
-            for (int j = 1; j <= stars; j++) {
-                System.out.print(" * ");
-            }
-            System.out.println();
-        }
+        
     }
 }
--- DSA-JAVA/Patterns.java
+++ DSA-JAVA/Patterns.java
@@ -68,28 +68,44 @@ public class Patterns {
     // }
     // }
 
-    public static void diamond(int n) {
-        // 1st half
+    // public static void diamond(int n) {
+    //     // 1st half
+    //     for (int i = 1; i <= n; i++) {
+    //         // spaces
+    //         for (int j = 1; j <= (n - i); j++) {
+    //             System.out.print("  ");
+    //         }
+    //         // stars
+    //         for (int j = 1; j <= (2 * i) - 1; j++) {
+    //             System.out.print("* ");
+    //         }
+    //         System.out.println();
+    //     }
+    //     // 2nd half
+    //     for (int i = n; i >= 1; i--) {
+    //         // spaces
+    //         for (int j = 1; j <= (n - i); j++) {
+    //             System.out.print("  ");
+    //         }
+    //         // stars
+    //         for (int j = 1; j <= (2 * i) - 1; j++) {
+    //             System.out.print("* ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    public static void palindromic_pattern (int n){
         for (int i = 1; i <= n; i++) {
-            // spaces
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print("  ");
+            int spaces = n - i;
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
             }
-            // stars
-            for (int j = 1; j <= (2 * i) - 1; j++) {
-                System.out.print("* ");
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
             }
-            System.out.println();
-        }
-        // 2nd half
-        for (int i = n; i >= 1; i--) {
-            // spaces
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print("  ");
-            }
-            // stars
-            for (int j = 1; j <= (2 * i) - 1; j++) {
-                System.out.print("* ");
+            for(int j=i-1; j>=1; j--){
+                System.out.print(j);
             }
             System.out.println();
         }
@@ -99,6 +115,7 @@ public class Patterns {
         // butterfly(4);
         // solid_rhombus(5);
         // hollow_rhombus(5);
-        diamond(3);
+        // diamond(3);
+        palindromic_pattern(5);
     }
 }
