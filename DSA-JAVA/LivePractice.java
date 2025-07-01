abf048cc89b6b90680e05d78e3969963da7c4733
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-01 17:25:17 +0530

    hourglass pattern

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -2,31 +2,27 @@
 
 class LivePractice {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 3;
         for (int i = n; i >= 1; i--) {
+            int stars = 2 * i;
             int spaces = n - i;
-            for(int j=1; j<=spaces; j++){
-                System.out.print(" ");
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("   ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
-            for(int j=i-1; j>=1; j--){
-                System.out.print("*");
+            for (int j = 1; j <= stars; j++) {
+                System.out.print(" * ");
             }
             System.out.println();
         }
         //reverse
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
+            int stars = 2 * i;
             int spaces = n - i;
-            for(int j=1; j<=spaces; j++){
-                System.out.print(" ");
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("   ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
-            }
-            for(int j=i-1; j>=1; j--){
-                System.out.print("*");
+            for (int j = 1; j <= stars; j++) {
+                System.out.print(" * ");
             }
             System.out.println();
         }
