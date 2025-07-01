c630680561a7e0d6165063966d7929ad467b5430
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-01 12:00:38 +0530

    diamond pattern generation

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -1,27 +1,29 @@
 import java.util.*;
 
 class LivePractice {
-
-    static int binaryToDecimal(int n) {
-        int mul = 1;
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum = sum + digit * mul;
-            n = n / 10;
-            mul = mul * 2;
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
-        int result1 = binaryToDecimal(101);
-        System.out.println("101 : " + result1);
-
-        int result2 = binaryToDecimal(111);
-        System.out.println("111 : " + result2);
-
-        int result3 = binaryToDecimal(1000);
-        System.out.println("1000 : " + result3);
+        int n = 3;
+        for (int i = 1; i <= n; i++) {
+            int stars = 2 * i - 1;
+            int spaces = n - i;
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            int stars = 2 * i - 1;
+            int spaces = n - i;
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= stars; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
     }
 }
