c5aa71c6c2b302018740e00a4f7e5f10f61d56c0
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-01 17:09:12 +0530

    Implement full diamond pattern generation in LivePractice.java

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -2,6 +2,33 @@ import java.util.*;
 
 class LivePractice {
     public static void main(String[] args) {
-        
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            int spaces = n - i;
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=i-1; j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //reverse
+        for (int i = 2; i <= n; i++) {
+            int spaces = n - i;
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            for(int j=i-1; j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
