fd52da0cad251d75867d872fbfcae5777eafd1ea
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-03 16:54:49 +0530

    Add functionality to find the smallest number in an array

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class ArraysCC {
     public static int getLargest(int numbers[]) {
         int largest = Integer.MIN_VALUE; // -infinity
+        int smallest = Integer.MAX_VALUE; // infinity
 
         for (int i = 0; i < numbers.length; i++) {
             if (largest < numbers[i]) {
                 largest = numbers[i];
             }
+            if(smallest > numbers[i]){
+                smallest = numbers[i];
+            }
         }
+        System.out.println("Smallest value is : " + smallest);
         return largest;
     }
 
