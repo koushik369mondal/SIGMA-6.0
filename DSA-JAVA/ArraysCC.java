70721f3ca785624ebf4927a06e0c078b68338186
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-03 23:55:36 +0530

    Reverse an Array

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -1,28 +1,30 @@
 import java.util.*;
 
 public class ArraysCC {
-    public static int binarySearch(int numbers[], int key) {
-        int start = 0, end = numbers.length - 1;
+    public static void reverse(int numbers[]) {
+        int first = 0, last = numbers.length - 1;
 
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        while (first <= last) {
+            // swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
 
-            // comparison
-            if (numbers[mid] == key) { // found
-                return mid;
-            }
-            if (numbers[mid] < key) { // right
-                start = mid + 1;
-            } else { // left
-                end = mid - 1;
-            }
+            first++;
+            last--;
         }
-        return -1;
+
     }
 
     public static void main(String[] args) {
         int numbers[] = { 2, 4, 6, 8, 10, 12, 14 };
-        int key = 10;
-        System.out.println("index for key is : " + binarySearch(numbers, key));
+
+        reverse(numbers);
+
+        //print
+        for (int i = 0; i < numbers.length; i++) {
+            System.err.print(numbers[i] + " ");
+        }
+        System.err.println();
     }
 }
