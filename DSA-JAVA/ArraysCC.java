b07ab77500aeab510f883a04c5bd691aa1c52032
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-04 00:17:11 +0530

    Pairs in Array

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -1,33 +1,18 @@
 import java.util.*;
 
 public class ArraysCC {
-    // Method to reverse array
-    public static void reverse(int numbers[]) {
-        int first = 0;
-        int last = numbers.length - 1;
-
-        while (first <= last) {
-            // Swap elements
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
-
-            // Move pointers
-            first++;
-            last--;
+    public static void printPairs(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i]; // 2, 4, 6, 8, 10
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+            }
+            System.out.println();
         }
     }
 
-    // Main method
     public static void main(String[] args) {
-        int numbers[] = { 2, 4, 6, 8, 10, 12, 14 };
-
-        reverse(numbers); // Call reverse
-
-        // Print reversed array
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printPairs(numbers);
     }
 }
