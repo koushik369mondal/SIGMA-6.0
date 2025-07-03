4885a5eb2e14ece3c4a623a01afa1768d92ac21e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-03 08:52:00 +0530

    length of the array

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -8,17 +8,19 @@ public class ArraysCC {
         // int phy;
         // phy = sc.nextInt();
 
-        marks[0] = sc.nextInt(); // phy
-        marks[1] = sc.nextInt(); // chem
-        marks[2] = sc.nextInt(); // math
+        System.out.println("Length of the array: " + marks.length);
 
-        System.out.println("Physics: " + marks[0]);
-        System.out.println("Chemistry: " + marks[1]);
-        System.out.println("Math: " + marks[2]);
-        // System.out.println("Not Defined: " + marks[9]);
+        // marks[0] = sc.nextInt(); // phy
+        // marks[1] = sc.nextInt(); // chem
+        // marks[2] = sc.nextInt(); // math
 
-        int percentage = ((marks[0] + marks[1] + marks[2]) / 3);
-        System.out.println("Percentage: " + percentage);
+        // System.out.println("Physics: " + marks[0]);
+        // System.out.println("Chemistry: " + marks[1]);
+        // System.out.println("Math: " + marks[2]);
+        // // System.out.println("Not Defined: " + marks[9]);
+
+        // int percentage = ((marks[0] + marks[1] + marks[2]) / 3);
+        // System.out.println("Percentage: " + percentage);
     }
 
 }
