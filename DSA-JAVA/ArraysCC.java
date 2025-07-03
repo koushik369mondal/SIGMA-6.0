556e9bf9d4b44df8f95e11e60c609b316c9b2857
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-03 08:45:17 +0530

    implement user input for subject marks

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -2,11 +2,20 @@ import java.util.*;
 
 public class ArraysCC {
     public static void main(String[] args) {
-        int marks[] = new int[50];
-        int num[] = {1, 2, 3};
-        double floatNum[] = { 1.2, 2.3, 3.4 };
-        char ch[] = { 'a', 'b', 'c' };
-        String fruits[] = { "apple", "mango", "orange" };
+        int marks[] = new int[100];
+
+        Scanner sc = new Scanner(System.in);
+        // int phy;
+        // phy = sc.nextInt();
+
+        marks[0] = sc.nextInt(); //phy
+        marks[1] = sc.nextInt(); //chem
+        marks[2] = sc.nextInt(); //math
+
+        System.out.println("Physics: " + marks[0]);
+        System.out.println("Chemistry: " + marks[1]);
+        System.out.println("Math: " + marks[2]);
+        System.out.println("Not Defined: " + marks[9]);
     }
 
 }
