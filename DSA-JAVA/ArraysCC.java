5e65cf1903ab3004ba7229f06bcbc7b5fd8bc155
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-04 20:46:10 +0530

    Max SubArray Sum - 11 (Prefix Sum)

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -4,17 +4,21 @@ public class ArraysCC {
     public static void maxSubArraysSum(int numbers[]) {
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+
+        prefix[0] = numbers[0];
+        // calculate prefix array
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
 
         for (int i = 0; i < numbers.length; i++) {
             int start = i;
             for (int j = i; j < numbers.length; j++) {
                 int end = j;
-                currSum = 0;
-                for (int k = start; k <= end; k++) { // print
-                    // subArray sum
-                    currSum += numbers[k];
-                }
-                System.out.println(currSum);
+
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+
                 if (maxSum < currSum) {
                     maxSum = currSum;
                 }
@@ -24,7 +28,7 @@ public class ArraysCC {
     }
 
     public static void main(String[] args) {
-        int numbers[] = { 2, 4, 6, 8, 10 };
+        int numbers[] = { 1, -2, 6, -1, 3 };
         maxSubArraysSum(numbers);
     }
 }
