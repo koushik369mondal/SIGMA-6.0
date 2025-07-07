c6512030770a0f3c1c9eb57b38023f437a0b5e3e
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-07 23:28:31 +0530

    implement buy and sell stocks functionality

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -1,38 +1,22 @@
 import java.util.*;
 
 public class ArraysCC {
-    public static int trappedRainWater(int height[]) {
-        int n = height.length;
-        // calculate left max boundary - array
-        int leftMax[] = new int[n];
-        leftMax[0] = height[0];
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for(int i=0; i<prices.length; i++){
+            if(buyPrice < prices[i]) {  //profit
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
         }
-
-        // calculate right max boundary - array
-        int rightMax[] = new int[n];
-        rightMax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
-        }
-
-        int trappedWater = 0;
-        // loop
-        for (int i = 0; i < n; i++) {
-            // waterLevel = min(leftMax bound, rightMax bound)
-            int waterLevel = Math.min(leftMax[i], rightMax[i]);
-
-            // trapped water = waterLevel - height[i]
-            trappedWater += waterLevel - height[i];
-        }
-
-        return trappedWater;
+        return maxProfit;
     }
 
-    public static void main(String[] args) {
-        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
-        System.out.println(trappedRainWater(height));
-        ;
+    public static void main(String[] args) { 
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndSellStocks(prices));
     }
 }
