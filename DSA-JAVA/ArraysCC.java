0ced645472bda6705a1d5fd2c8148038161d8f1c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-16 18:42:39 +0530

    containsDuplicate method in LivePractice for duplicate detection.

--- DSA-JAVA/ArraysCC.java
+++ DSA-JAVA/ArraysCC.java
@@ -6,7 +6,7 @@ public class ArraysCC {
         int maxProfit = 0;
         for(int i=0; i<prices.length; i++){
             if(buyPrice < prices[i]) {  //profit
-                int profit = prices[i] - buyPrice;
+                int profit = prices[i] - buyPrice; //todays profit
                 maxProfit = Math.max(maxProfit, profit);
             } else {
                 buyPrice = prices[i];
--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -1,7 +1,19 @@
-// import java.util.*;
+import java.util.*;
+import java.lang.reflect.Array;
 
 class LivePractice {
-    public static void main(String[] args) {
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 4, 2 };
+        System.out.println("Contains Duplicate: " + containsDuplicate(nums));
     }
 }
