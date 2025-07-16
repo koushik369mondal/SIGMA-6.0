abb033f307e0306fd3b8a72ce8a9b5ef33a4747c
Kaushik Mandal <koushik369mondal@gmail.com>
2025-07-16 19:06:05 +0530

    containsDuplicate using character

--- DSA-JAVA/LivePractice.java
+++ DSA-JAVA/LivePractice.java
@@ -2,10 +2,10 @@ import java.util.*;
 import java.lang.reflect.Array;
 
 class LivePractice {
-    public static boolean containsDuplicate(int[] nums) {
+    public static boolean containsDuplicate(char[] nums) {
         Arrays.sort(nums);
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] == nums[i+1]){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
@@ -13,7 +13,7 @@ class LivePractice {
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 4, 2 };
+        char[] nums = { 'a', 'b', 'c', 'd' };
         System.out.println("Contains Duplicate: " + containsDuplicate(nums));
     }
 }
