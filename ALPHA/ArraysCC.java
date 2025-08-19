d4931aac119b5406b4da665a475d255366274fe6
Kaushik Mandal <koushik369mondal@gmail.com>
2025-08-19 12:02:11 +0530

    style: format code for consistency in ArraysCC.java

--- ALPHA/ArraysCC.java
+++ ALPHA/ArraysCC.java
@@ -4,9 +4,9 @@ public class ArraysCC {
     public static int buyAndSellStocks(int prices[]) {
         int buyPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
-        for(int i=0; i<prices.length; i++){
-            if(buyPrice < prices[i]) {  //profit
-                int profit = prices[i] - buyPrice; //todays profit
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) { // profit
+                int profit = prices[i] - buyPrice; // todays profit
                 maxProfit = Math.max(maxProfit, profit);
             } else {
                 buyPrice = prices[i];
@@ -15,7 +15,7 @@ public class ArraysCC {
         return maxProfit;
     }
 
-    public static void main(String[] args) { 
+    public static void main(String[] args) {
         int prices[] = { 7, 1, 5, 3, 6, 4 };
         System.out.println(buyAndSellStocks(prices));
     }
