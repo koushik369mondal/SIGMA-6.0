aaefe80015820f3c968bbc34e2935fd1eb4f8967
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-31 15:25:48 +0530

    Print out the sum of the numbers inthe second row of the “nums” array.

--- ALPHA/2DArray.java
+++ ALPHA/2DArray.java
@@ -2,23 +2,40 @@ import java.util.*;
 
 class Ideone {
     public static void main(String[] args) {
-        // Question 1 
+        // Question 1
         // int arr[][] = {
-        //         { 5, 6, 7 },
-        //         { 4, 9, 10 },
-        //         { 8, 7, 5 },
-        //         { 7, 7, 6 }
+        // { 5, 6, 7 },
+        // { 4, 9, 10 },
+        // { 8, 7, 5 },
+        // { 7, 7, 6 }
         // };
         // int n = arr.length;
         // int m = arr[0].length;
         // int count = 0;
         // for( int i=0; i<n; i++){
-        //     for( int j=0; j<m; j++){
-        //         if(arr[i][j] == 7){
-        //             count++;
-        //         }
-        //     }
+        // for( int j=0; j<m; j++){
+        // if(arr[i][j] == 7){
+        // count++;
+        // }
+        // }
         // }
         // System.out.println("Count of 7 is: " + count);
+
+        // Question 2
+        int arr[][] = {
+                { 10, 20, 30, 40 },
+                { 7, 9, 8, 6 },
+                { 4, 3, 2, 1 },
+                { 7, 8, 6, 6 },
+                { 9, 9, 8, 8 }
+        };
+        int n = arr.length;
+        int m = arr[0].length;
+        int sum = 0;
+        // 2nd row index = 1
+        for(int j=0; j<m; j++){
+            sum = sum + arr[1][j];
+        }
+        System.out.println("Sum of 2nd row is: " + sum);
     }
 }
