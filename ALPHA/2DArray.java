083e576e9f39552fb96ae1d0fd089ae88716785a
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-31 16:01:01 +0530

    Comment out unused code related to array initialization and sum calculation in 2DArray.java

--- ALPHA/2DArray.java
+++ ALPHA/2DArray.java
@@ -22,24 +22,24 @@ class Ideone {
         // System.out.println("Count of 7 is: " + count);
 
         // Question 2
-        int arr[][] = {
-                { 10, 20, 30, 40 },
-                { 7, 9, 8, 6 },
-                { 4, 3, 2, 1 },
-                { 7, 8, 6, 6 },
-                { 9, 9, 8, 8 }
-        };
-        int n = arr.length;
-        int m = arr[0].length;
-        int sum = 0;
-        // 2nd row index = 1
-        // for(int i=0; i<m; i++){
-        //     sum = sum + arr[1][i];
+        // int arr[][] = {
+        //         { 10, 20, 30, 40 },
+        //         { 7, 9, 8, 6 },
+        //         { 4, 3, 2, 1 },
+        //         { 7, 8, 6, 6 },
+        //         { 9, 9, 8, 8 }
+        // };
+        // int n = arr.length;
+        // int m = arr[0].length;
+        // int sum = 0;
+        // // 2nd row index = 1
+        // // for(int i=0; i<m; i++){
+        // //     sum = sum + arr[1][i];
+        // // }
+        // // 3rd col means j=2 col
+        // for(int i=0; i<n; i++) {
+        //     sum = sum + arr[i][2];
         // }
-        // 3rd col means j=2 col
-        for(int i=0; i<n; i++) {
-            sum = sum + arr[i][2];
-        }
-        System.out.println("Sum of 3rd column is: " + sum);
+        // System.out.println("Sum of 3rd column is: " + sum);
     }
 }
