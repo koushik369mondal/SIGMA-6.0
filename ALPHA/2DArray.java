6d6ff006196220b71cdf0982a58bcba08b4c2602
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-31 18:29:00 +0530

    Uncomment and implement functionality for counting occurrences of 7 and summing the 3rd column in 2DArray.java

--- ALPHA/2DArray.java
+++ ALPHA/2DArray.java
@@ -1,55 +1,59 @@
 import java.util.*;
 
 class Ideone {
-    public static void main(String[] args) {
-        // Question 1 - Count occurrences of 7 in 2D array
-        // int arr[][] = {
-        // { 5, 6, 7 },
-        // { 4, 9, 10 },
-        // { 8, 7, 5 },
-        // { 7, 7, 6 }
-        // };
-        // int n = arr.length;
-        // int m = arr[0].length;
-        // int count = 0;
-        // for( int i=0; i<n; i++){
-        // for( int j=0; j<m; j++){
-        // if(arr[i][j] == 7){
-        // count++;
-        // }
-        // }
-        // }
-        // System.out.println("Count of 7 is: " + count);
+    // Question 1 - Count occurrences of 7 in 2D array
+    public static void question1() {
+        int arr[][] = {
+                { 5, 6, 7 },
+                { 4, 9, 10 },
+                { 8, 7, 5 },
+                { 7, 7, 6 }
+        };
+        int n = arr.length;
+        int m = arr[0].length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 7) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Count of 7 is: " + count);
+    }
 
-        // Question 2 - Sum of 3rd column
-        // int arr[][] = {
-        // { 10, 20, 30, 40 },
-        // { 7, 9, 8, 6 },
-        // { 4, 3, 2, 1 },
-        // { 7, 8, 6, 6 },
-        // { 9, 9, 8, 8 }
-        // };
-        // int n = arr.length;
-        // int m = arr[0].length;
-        // int sum = 0;
-        // // 2nd row index = 1
-        // // for(int i=0; i<m; i++){
-        // // sum = sum + arr[1][i];
-        // // }
-        // // 3rd col means j=2 col
-        // for(int i=0; i<n; i++) {
-        // sum = sum + arr[i][2];
+    // Question 2 - Sum of 3rd column
+    public static void question2() {
+        int arr[][] = {
+                { 10, 20, 30, 40 },
+                { 7, 9, 8, 6 },
+                { 4, 3, 2, 1 },
+                { 7, 8, 6, 6 },
+                { 9, 9, 8, 8 }
+        };
+        int n = arr.length;
+        int m = arr[0].length;
+        int sum = 0;
+        // 2nd row index = 1
+        // for(int i=0; i<m; i++){
+        // sum = sum + arr[1][i];
         // }
-        // System.out.println("Sum of 3rd column is: " + sum);
+        // 3rd col means j=2 col
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i][2];
+        }
+        System.out.println("Sum of 3rd column is: " + sum);
+    }
 
-        // Question 3 - Transpose of a matrix
+    // Question 3 - Transpose of a matrix
+    public static void question3() {
         int arr[][] = {
                 { 5, 5, 7 },
                 { 1, 2, 3 },
                 { 10, 15, 39 },
                 { 60, 70, 80 }
         };
-        int n = arr.length; 
+        int n = arr.length;
         int m = arr[0].length;
         int transpose[][] = new int[m][n];
         // iterate in original matrix and transfer elements to transpose matrix
@@ -65,5 +69,11 @@ class Ideone {
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        question1();
+        question2();
+        question3();
     }
 }
