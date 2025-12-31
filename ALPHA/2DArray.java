a58dc143b2088a87b5842114c36d3dbb9a09f461
Kaushik Mandal <koushik369mondal@gmail.com>
2025-12-31 11:30:20 +0530

    Add 2DArray.java to count occurrences of the number 7 in a 2D array

---
+++ ALPHA/2DArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Ideone {
+    public static void main(String[] args) {
+        int arr[][] = {
+                { 5, 6, 7 },
+                { 4, 9, 10 },
+                { 8, 7, 5 },
+                { 7, 7, 6 }
+        };
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int count = 0;
+        for( int i=0; i<n; i++){
+            for( int j=0; j<m; j++){
+                if(arr[i][j] == 7){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Count of 7 is: " + count);
+    }
+}
