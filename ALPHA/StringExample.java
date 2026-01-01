78721d5297f2778f221fbb6aef4c7277d67844e7
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-01 17:01:46 +0530

    print first character of full name in StringExample.java

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -12,13 +12,13 @@ public class StringExample{
         // name = sc.nextLine();
         // System.out.println("Hello " + name);
 
-        String name = "Tony Stark";
-        System.out.println(name.length());
+        // String name = "Tony Stark";
+        // System.out.println(name.length());
 
         //concatenation
         String firstName = "Kaushik";
         String lastName = "Mandal";
         String fullName = firstName + " " + lastName;
-        System.out.println(fullName);
+        System.out.println(fullName.charAt(0));
     }
 }
