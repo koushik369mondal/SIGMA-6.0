863c7dcbde1adbe15892ac927594a8e4831adc81
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-01 16:31:37 +0530

    Add StringExample.java with basic string manipulation and user input functionality

---
+++ ALPHA/StringExample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class StringExample{
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        //Strings are IMMUTABLE
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.next();
+        System.out.println("Hello " + name);
+    }
+}
