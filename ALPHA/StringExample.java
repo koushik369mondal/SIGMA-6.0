4ba2659ca2493fc33521903c4ed44d861741c80e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-01 16:37:55 +0530

    Find length of the string

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 public class StringExample{
     public static void main(String[] args) {
-        char arr[] = {'a', 'b', 'c', 'd'};
-        String str = "abcd";
-        String str2 = new String("xyz");
+        // char arr[] = {'a', 'b', 'c', 'd'};
+        // String str = "abcd";
+        // String str2 = new String("xyz");
 
-        //Strings are IMMUTABLE
-        Scanner sc = new Scanner(System.in);
-        String name;
-        name = sc.nextLine();
-        System.out.println("Hello " + name);
+        // //Strings are IMMUTABLE
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println("Hello " + name);
+
+        String name = "Tony Stark";
+        System.out.println(name.length());
     }
 }
