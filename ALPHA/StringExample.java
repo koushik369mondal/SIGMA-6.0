c4e2f63bdf83696527c5781a0b8bb9056a15e28c
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-02 09:31:45 +0530

    add subString method to extract a substring from a given string

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -111,12 +111,23 @@ public class StringExample {
         }
     }
 
+    public static String subString(String str, int si, int ei){
+        String substr = "";
+        for(int i = si; i < ei; i++){
+            substr += str.charAt(i);
+        }
+        return substr;
+    }
+
     public static void main(String[] args) {
         // basicStringExamples();
         // stringLengthExample();
         // stringConcatenationExample();
         // isPalindrome();
         // getShortestPath();
-        compare();
+        // compare();
+        String str = "HelloWorld";
+        String result = subString(str, 0, 5);
+        System.out.println("Substring: " + result);
     }
 }
