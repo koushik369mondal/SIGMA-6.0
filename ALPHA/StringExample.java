c01b921f6afdc97b1639b548e04b853e3eea93ed
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-02 08:40:40 +0530

    refactor getShortestPath method to remove parameter and print result directly

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -59,7 +59,8 @@ public class StringExample {
         System.out.println(true);
     }
 
-    public static float getShortestPath(String path) {
+    public static void getShortestPath() {
+        String path = "WNEENESENNN";
         int x = 0, y = 0;
         for (int i = 0; i < path.length(); i++) {
             char dir = path.charAt(i);
@@ -82,7 +83,8 @@ public class StringExample {
         }
         int X2 = x * x;
         int Y2 = y * y;
-        return (float) Math.sqrt(X2 + Y2);
+        float shortestPath = (float) Math.sqrt(X2 + Y2);
+        System.out.println("Shortest Path: " + shortestPath);
     }
 
     public static void main(String[] args) {
@@ -90,8 +92,6 @@ public class StringExample {
         // stringLengthExample();
         // stringConcatenationExample();
         // isPalindrome();
-        
-        String path = "WNEENESENNN";
-        System.out.println(getShortestPath(path));
+        getShortestPath();
     }
 }
