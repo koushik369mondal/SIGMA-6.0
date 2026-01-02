675d09b00957faeb0ad66ceaa76b0060d775a7f0
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-02 08:35:26 +0530

    implement getShortestPath method to calculate distance based on directional input

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -59,10 +59,39 @@ public class StringExample {
         System.out.println(true);
     }
 
+    public static float getShortestPath(String path) {
+        int x = 0, y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            char dir = path.charAt(i);
+            // South
+            if (dir == 'S') {
+                y--;
+            }
+            // North
+            else if (dir == 'N') {
+                y++;
+            }
+            // West
+            else if (dir == 'W') {
+                x--;
+            }
+            // East
+            else {
+                x++;
+            }
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+        return (float) Math.sqrt(X2 + Y2);
+    }
+
     public static void main(String[] args) {
         // basicStringExamples();
         // stringLengthExample();
         // stringConcatenationExample();
-        isPalindrome();
+        // isPalindrome();
+        
+        String path = "WNEENESENNN";
+        System.out.println(getShortestPath(path));
     }
 }
