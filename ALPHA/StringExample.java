c798b1bc8b9646eaa622e163621b659878cbb39a
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-05 09:26:12 +0530

    add toUpperCase method to convert the first character of each word to uppercase

--- ALPHA/StringExample.java
+++ ALPHA/StringExample.java
@@ -148,7 +148,26 @@ public class StringExample {
         }
         // O(26) time complexity
         // O(26 * n^2) time complexity for string concatenation without StringBuilder
+        System.out.println(sb.toString());
         System.out.println(sb.length());
+    }
+
+    public static String toUpperCase(String str){
+        StringBuilder sb = new StringBuilder("");
+
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(i) == ' ' && i<str.length()-1){
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args) {
@@ -161,6 +180,8 @@ public class StringExample {
         // subString();
         // subStringBuildIn();
         // largestString();
-        stringBuilder();
+        // stringBuilder();
+        String str = "Hi, I am kaushik mandal.";
+        System.out.println(toUpperCase(str));
     }
 }
