8e7154a21bdc18b0c90685459347acdb78bc91b9
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-18 08:16:42 +0530

    add bitwiseOperators method to demonstrate bitwise operations

--- ALPHA/BitManipulation.java
+++ ALPHA/BitManipulation.java
@@ -2,6 +2,15 @@
 
 public class BitManipulation {
 
+    public static void bitwiseOperators(int a, int b){
+        System.out.println("a & b: " + (a & b)); // AND
+        System.out.println("a | b: " + (a | b)); // OR
+        System.out.println("a ^ b: " + (a ^ b)); // XOR
+        System.out.println("~a: " + (~a));       // NOT
+        System.out.println("a << 1: " + (a << 1)); // Left Shift
+        System.out.println("a >> 1: " + (a >> 1)); // Right Shift
+    }
+
     public static void oddOrEven(int n){
         int bitMask = 1;
         if((n & bitMask) == 0){
@@ -12,6 +21,7 @@ public class BitManipulation {
     }
 
     public static void main(String[] args) {
+        bitwiseOperators(5, 3);
         oddOrEven(4);
         oddOrEven(12);
         oddOrEven(15);
