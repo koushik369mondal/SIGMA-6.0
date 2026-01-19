7bb33f145d028d3fe8a2b7b3445467c3fa58dc0d
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-19 09:39:59 +0530

    Get ith bit

--- ALPHA/BitManipulation.java
+++ ALPHA/BitManipulation.java
@@ -2,28 +2,37 @@
 
 public class BitManipulation {
 
-    public static void bitwiseOperators(int a, int b){
+    public static void bitwiseOperators(int a, int b) {
         System.out.println("a & b: " + (a & b)); // AND
         System.out.println("a | b: " + (a | b)); // OR
         System.out.println("a ^ b: " + (a ^ b)); // XOR
-        System.out.println("~a: " + (~a));       // NOT
+        System.out.println("~a: " + (~a)); // NOT
         System.out.println("a << 1: " + (a << 1)); // Left Shift
         System.out.println("a >> 1: " + (a >> 1)); // Right Shift
     }
 
-    public static void oddOrEven(int n){
+    public static void oddOrEven(int n) {
         int bitMask = 1;
-        if((n & bitMask) == 0){
+        if ((n & bitMask) == 0) {
             System.out.println("Even Number");
         } else {
             System.out.println("Odd Number");
         }
     }
 
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     public static void main(String[] args) {
-        bitwiseOperators(5, 3);
-        oddOrEven(4);
-        oddOrEven(12);
-        oddOrEven(15);
+        // bitwiseOperators(5, 3);
+        // oddOrEven(12);
+        // oddOrEven(15);
+        System.out.println(getIthBit(10, 2)); // Here, 10 in binary is 1010
     }
 }
