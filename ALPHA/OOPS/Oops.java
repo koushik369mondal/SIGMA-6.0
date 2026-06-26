3c6fcd09ed1f95e7be2145d369b5bc3fb0d31c5f
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-26 11:19:53 +0530

    Classes and objects

---
+++ ALPHA/OOPS/Oops.java
@@ -0,0 +1,50 @@
+package OOPS;
+
+public class Oops {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        pen1.setColor("Blue");
+        pen1.setTip(5);
+        System.out.println("Pen Color: " + pen1.getColor());
+        System.out.println("Pen Tip Size: " + pen1.getTip());
+
+        Student student1 = new Student();
+        student1.name = "John";
+        student1.age = 20;
+        student1.calcPercentage(85, 90, 80);
+        System.out.println("Student Name: " + student1.name);
+        System.out.println("Student Age: " + student1.age);
+        System.out.println("Student Percentage: " + student1.percentage);
+    }
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTip(int tip) {
+        this.tip = tip;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getTip() {
+        return this.tip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage(int phy, int chem, int math) {
+        percentage = (phy + chem + math) / 3;
+    }
+}
