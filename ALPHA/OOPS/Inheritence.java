6dfb1a135e3f19a49a9f03157d59d57379f5fa85
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-28 22:17:47 +0530

    improve code structure of Inheritence

--- ALPHA/OOPS/Inheritence.java
+++ ALPHA/OOPS/Inheritence.java
@@ -2,51 +2,89 @@ package OOPS;
 
 public class Inheritence {
     public static void main(String[] args) {
-        Fish shark = new Fish();
+
+        Shark shark = new Shark();
         shark.eat();
         shark.breathe();
         shark.swim();
+
+        System.out.println();
+
         Dog dobby = new Dog();
         dobby.eat();
+        dobby.walk();
+        dobby.bark();
         dobby.legs = 4;
         System.out.println("Dobby has " + dobby.legs + " legs");
+
+        System.out.println();
+
+        Peacock peacock = new Peacock();
+        peacock.eat();
+        peacock.fly();
+        peacock.dance();
     }
 }
 
-// Base class
-class Animal{
+// ================= Base Class =================
+
+class Animal {
     String color;
 
-    void eat(){
+    void eat() {
         System.out.println("Animal is eating");
     }
 
-    void breathe(){
+    void breathe() {
         System.out.println("Animal is breathing");
     }
 }
 
-// Derived class / subclass 
-class Fish extends Animal{
+// ================= Level 1 =================
+
+class Fish extends Animal {
     int fins;
 
-    void swim(){
+    void swim() {
         System.out.println("Fish is swimming");
     }
 }
 
-class Mammls extends Animal{
+class Mammal extends Animal {
     int legs;
 
-    void run(){
-        System.out.println("Mammal is running");
+    void walk() {
+        System.out.println("Mammal is walking");
     }
 }
 
-class Dog extends Mammls{
+class Bird extends Animal {
+
+    void fly() {
+        System.out.println("Bird is flying");
+    }
+}
+
+// ================= Level 2 =================
+
+class Shark extends Fish {
+
+    void hunt() {
+        System.out.println("Shark is hunting");
+    }
+}
+
+class Dog extends Mammal {
     String breed;
 
-    void bark(){
+    void bark() {
         System.out.println("Dog is barking");
+    }
+}
+
+class Peacock extends Bird {
+
+    void dance() {
+        System.out.println("Peacock is dancing");
     }
 }
