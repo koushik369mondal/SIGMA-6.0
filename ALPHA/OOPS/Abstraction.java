668b18ed474a94dc28ea0122b955e68c81129707
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-29 12:29:14 +0530

    Organize Abstraction

--- ALPHA/OOPS/Abstraction.java
+++ ALPHA/OOPS/Abstraction.java
@@ -1,28 +1,54 @@
 package OOPS;
 
 public class Abstraction {
+
     public static void main(String[] args) {
+
+        // Horse Object
         Horse h = new Horse();
         h.eat();
         h.walk();
-        System.out.println(h.color);
-        h.changeColor();
-        System.out.println(h.color);
+        System.out.println("Horse Color: " + h.color);
 
+        h.changeColor();
+        System.out.println("Horse New Color: " + h.color);
+
+        System.out.println();
+
+        // Chicken Object
         Chicken c = new Chicken();
         c.eat();
         c.walk();
-        System.out.println(c.color);
+        System.out.println("Chicken Color: " + c.color);
+
         c.changeColor();
-        System.out.println(c.color);
+        System.out.println("Chicken New Color: " + c.color);
+
+        System.out.println();
+
+        // Multilevel Inheritance
+        // Animal -> Horse -> Mustang
+        Mustang m = new Mustang();
+        m.eat();
+        m.walk();
+        System.out.println("Mustang Color: " + m.color);
+
+        m.changeColor();
+        System.out.println("Mustang New Color: " + m.color);
     }
 }
 
+// ===================================================
+// Abstract Parent Class
+// ===================================================
+
 abstract class Animal {
+
     String color;
 
     Animal() {
         color = "brown";
+        System.out.println("Animal constructor called");
     }
 
     void eat() {
@@ -32,22 +58,55 @@ abstract class Animal {
     abstract void walk();
 }
 
+// ===================================================
+// Horse Class
+// ===================================================
+
 class Horse extends Animal {
+
+    Horse() {
+        System.out.println("Horse constructor called");
+    }
+
     void changeColor() {
         color = "black brown";
     }
 
+    @Override
     void walk() {
         System.out.println("Horse walks on 4 legs");
     }
 }
 
+// ===================================================
+// Chicken Class
+// ===================================================
+
 class Chicken extends Animal {
+
     void changeColor() {
         color = "white";
     }
 
+    @Override
     void walk() {
         System.out.println("Chicken walks on 2 legs");
+    }
+}
+
+// ===================================================
+// Mustang Class (Multilevel Inheritance)
+// Animal -> Horse -> Mustang
+// ===================================================
+
+class Mustang extends Horse {
+
+    Mustang() {
+        System.out.println("Mustang constructor called");
+    }
+
+    @Override
+    void changeColor() {
+        color = "black";
     }
 }
