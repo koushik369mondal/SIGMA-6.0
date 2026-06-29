4b9994cca7109af0ea8d2ac796a92f7f0cca9d5e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-29 15:29:53 +0530

    Refactor Interfaces class to improve structure and organization of interface implementations

--- ALPHA/OOPS/Interfaces.java
+++ ALPHA/OOPS/Interfaces.java
@@ -1,12 +1,28 @@
 package OOPS;
 
 public class Interfaces {
-    public static void main(String[] args){
-        Queen q = new Queen();
-        q.moves();
+    public static void main(String[] args) {
+
+        // Multiple interfaces
+        Bear bear = new Bear();
+        bear.eatPlants();
+        bear.eatMeat();
+
+        System.out.println();
+
+        // Chess players
+        Queen queen = new Queen();
+        queen.moves();
+
+        Rook rook = new Rook();
+        rook.moves();
+
+        King king = new King();
+        king.moves();
     }
 }
 
+// Interfaces
 interface Herbivore {
     void eatPlants();
 }
@@ -15,33 +31,43 @@ interface Carnivore {
     void eatMeat();
 }
 
+interface ChessPlayer {
+    void moves();
+}
+
+// Classes
 class Bear implements Herbivore, Carnivore {
+
+    @Override
     public void eatPlants() {
         System.out.println("Bear eats plants");
     }
 
+    @Override
     public void eatMeat() {
         System.out.println("Bear eats meat");
     }
 }
 
-interface ChessPlayer {
-    void moves();
-}
-
 class Queen implements ChessPlayer {
+
+    @Override
     public void moves() {
         System.out.println("Queen can move in any direction");
     }
 }
 
 class Rook implements ChessPlayer {
+
+    @Override
     public void moves() {
         System.out.println("Rook can move in straight lines");
     }
 }
 
 class King implements ChessPlayer {
+
+    @Override
     public void moves() {
         System.out.println("King can move one square in any direction");
     }
