b1b790e371fb5f0a6a61979795491370a55c97b3
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-29 12:19:41 +0530

    Enhance Abstraction example with color change functionality for Horse and Chicken

--- ALPHA/OOPS/Abstraction.java
+++ ALPHA/OOPS/Abstraction.java
@@ -5,27 +5,48 @@ public class Abstraction {
         Horse h = new Horse();
         h.eat();
         h.walk();
+        System.out.println(h.color);
+        h.changeColor();
+        System.out.println(h.color);
 
         Chicken c = new Chicken();
         c.eat();
         c.walk();
+        System.out.println(c.color);
+        c.changeColor();
+        System.out.println(c.color);
     }
 }
 
 abstract class Animal {
+    String color;
+
+    Animal() {
+        color = "brown";
+    }
+
     void eat() {
         System.out.println("Animal eats");
     }
+
     abstract void walk();
 }
 
 class Horse extends Animal {
+    void changeColor() {
+        color = "black brown";
+    }
+
     void walk() {
         System.out.println("Horse walks on 4 legs");
     }
 }
 
 class Chicken extends Animal {
+    void changeColor() {
+        color = "white";
+    }
+
     void walk() {
         System.out.println("Chicken walks on 2 legs");
     }
