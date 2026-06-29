26779958f06602d5d075f8fad971545269215d7d
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-29 10:50:36 +0530

    Abstract class

---
+++ ALPHA/OOPS/Abstraction.java
@@ -0,0 +1,29 @@
+package OOPS;
+
+public class Abstraction {
+    public static void main(String[] args) {
+        Horse h = new Horse();
+        h.eat();
+        h.walk();
+    }
+}
+
+abstract class Animal {
+    void eat() {
+        System.out.println("Animal eats");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void walk() {
+        System.out.println("Horse walks on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    void walk() {
+        System.out.println("Chicken walks on 2 legs");
+    }
+}
