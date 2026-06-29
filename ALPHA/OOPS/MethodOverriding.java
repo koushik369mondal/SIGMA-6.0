cfbf0ea42158359514806651f0e8a41a5607ba7b
Kaushik Mandal <koushik369mondal@gmail.com>
2026-06-29 08:54:25 +0530

    Method Overriding

---
+++ ALPHA/OOPS/MethodOverriding.java
@@ -0,0 +1,21 @@
+package OOPS;
+
+public class MethodOverriding {
+    
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();    
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+class Deer extends Animal{
+    void eat(){
+        System.out.println("Deer is eating");
+    }
+}
