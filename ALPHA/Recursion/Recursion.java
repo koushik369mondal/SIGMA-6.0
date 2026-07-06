0c549282432a6cdec7faba625eb156c8ba04e9e6
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-06 18:00:54 +0530

    implement recursive decreasing print function in Recursion class

--- ALPHA/Recursion/Recursion.java
+++ ALPHA/Recursion/Recursion.java
@@ -2,13 +2,18 @@ package Recursion;
 
 public class Recursion {
     public static void printDec(int n){
-        if(n==0){
+        if(n==1){
             System.out.println(n);
             return;
         }
         System.out.print(n + " ");
         printDec(n-1);
     }
+
+    public static void printInc(int n){
+        
+    }
+
     public static void main(String[] args) {
         printDec(10);
     }
