207835c09d736eb549e3756947e7eb44250ae14e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-08-11 18:16:07 +0530

    add basic recursion implementations for printing numbers and calculating factorials

--- ALPHA/Recursion/Recursion.java
+++ ALPHA/Recursion/Recursion.java
@@ -12,16 +12,26 @@ public class Recursion {
 
     public static void printInc(int n){
         if(n == 1){
-            System.out.print(n + " ");
+            System.out.print(1 + " ");
             return;
         }
-        printInc(n-1);
         System.out.print(n + " ");
+        printInc(n-1);
+    }
+
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * fact(n-1);
+        return fn;
     }
 
     public static void main(String[] args) {
         // printDec(10);
-        printInc(10);
+        // printInc(10);
+        System.out.println(fact(5));;
     }
 }
 
