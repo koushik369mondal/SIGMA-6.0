9130b6b3a2e64f0d800ba06dda7728507cbe53b1
Kaushik Mandal <koushik369mondal@gmail.com>
2026-08-12 08:37:02 +0530

    Fibonacci using recursion

--- ALPHA/Recursion/Recursion.java
+++ ALPHA/Recursion/Recursion.java
@@ -37,11 +37,22 @@ public class Recursion {
         return sn;
     }
 
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
     public static void main(String[] args) {
         // printDec(10);
         // printInc(10);
         // System.out.println(fact(5));
-        System.out.println(sum(5));
+        // System.out.println(sum(5));
+        System.out.println(fib(10));
     }
 }
 
