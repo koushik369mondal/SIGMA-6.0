1d50124df29c8cf8fa492769fb33d4dbd899b483
Kaushik Mandal <koushik369mondal@gmail.com>
2026-09-18 20:20:02 +0530

    implement LinkedList class with basic node and manipulation methods

--- ALPHA/LinkedList/LinkedList.java
+++ ALPHA/LinkedList/LinkedList.java
@@ -68,9 +68,18 @@ public class LinkedList {
     }
 
     public int removeFirst() {
+        if (size == 0) {
+            System.out.println("LinkedList is empty");
+            return Integer.MIN_VALUE;
+        } else if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
         int val = head.data;
         head = head.next;
-        // size--;
+        size--;
         return val;
     }
 
